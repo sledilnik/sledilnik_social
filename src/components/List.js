39e50e049dd9ee6469f48fd856d05809d9f86a94
matejmeglic <matej@matejmeglic.com>
2020-12-13 12:43:29 +0100

    municipalities za silo

--- src/components/List.js
+++ src/components/List.js
@@ -8,6 +8,7 @@ const List = (props) => {
 
   const { stats } = props;
   const { municipalities } = props;
+  console.log(municipalities)
   const { patients } = props;
   if (!stats || stats.length === 0) return <p>Napaka: API ne vrača podatkov, refresh page !!!</p>;
 
@@ -90,9 +91,9 @@ const List = (props) => {
       <p className="text">
       <span role="img" aria-label='s'>➡️</span> Po krajih: 
       </p>
-      {/* <ul className="municipalities">      
+      <ul className="municipalities">      
       <span className={paint_mun}>
-      <Municipalities data={municipalities}></Municipalities></span></ul> */}
+      <Municipalities data={municipalities}></Municipalities></span></ul>
 
       {/* <p>Legenda trenda (primerjava današnjega stanja novookuženih glede na 7-dnevno povprečje):</p>
       <p>- Nižje od povprečja <img src='trend_down.svg' alt='trend_down' width='16px' height='auto' /></p>
