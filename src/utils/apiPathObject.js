3ebcf348d6252cec53e353c49fbeb37d8fbf01f6
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-06 16:03:22 +0100

    feat(utils): add api paths object

---
+++ src/utils/apiPathObject.js
@@ -0,0 +1,27 @@
+import getSledilnikApiPath from '../helpers/getSledilnikApiPath';
+import getDateBefore from '../helpers/getDateBefore';
+
+const dateFrom = getDateBefore().toISOString();
+
+const pathsParamsObject = {
+  stats: { from: dateFrom },
+  patients: { from: dateFrom },
+  municipalities: { from: dateFrom },
+  hospitals: {},
+  lab_tests: { from: dateFrom },
+  summary: {},
+};
+
+const pathsArray = Object.entries(pathsParamsObject);
+
+const apiPathObject = (paths = []) => {
+  return paths.reduce((acc, curVal) => {
+    const key = `${curVal[0]}Path`;
+    const path = curVal[0].replace('_', '-');
+    const params = curVal[1];
+    acc[key] = getSledilnikApiPath(path, params);
+    return acc;
+  }, {});
+};
+
+export default apiPathObject(pathsArray);
