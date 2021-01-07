cb99aad7a215a2a1a2b99bf32f734d16c8894082
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-07 13:51:50 +0100

    refactor(ThirdTweet): implement PatientsLine

--- src/components/Tweets/ThirdTweet.js
+++ src/components/Tweets/ThirdTweet.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Arrow from '../Arrow';
-import Separator from '../Separator';
-import Translate from '../Translate';
+
 import PerAgeLine from '../PerAgeLine';
 import MunicipalitiesLines from '../MunicipalitiesLines';
+import PatientsLine from '../PatientsLine';
 
 function ThirdTweet({
   check_second,
@@ -24,60 +23,11 @@ function ThirdTweet({
         todayPerAge={todayPerAge}
         yesterdayPerAge={yesterdayPerAge}
       />
-      <span className={check_second}>
-        <p className="text">
-          <Arrow /> Stanje po bolnišnicah:
-        </p>
-        <ul>
-          {patients[patients.length - 1] === undefined
-            ? 'NI PODATKOV'
-            : perHospitalChanges
-                .sort(
-                  (a, b) =>
-                    (b[1].inHospital.today || 0) - (a[1].inHospital.today || 0)
-                )
-                .map(hosp => {
-                  return hosp[1].inHospital.today === undefined ? (
-                    ''
-                  ) : (
-                    <li key={hosp[0]}>
-                      <span>
-                        <span className="bold">{hosp[2]}</span>:{' '}
-                        <span className="bold">
-                          <Separator number={hosp[1].inHospital.today} />
-                        </span>{' '}
-                        <Translate
-                          text={'oseba'}
-                          number={hosp[1].inHospital.today}
-                        ></Translate>{' '}
-                        (
-                        <span className="bold">
-                          +<Separator number={hosp[1].inHospital.in} /> -
-                          <Separator number={hosp[1].inHospital.out} />
-                        </span>
-                        ), EIT{' '}
-                        <span className="bold">
-                          <Separator number={hosp[1].icu.today} />
-                        </span>{' '}
-                        <Translate
-                          text={'oseba'}
-                          number={hosp[1].icu.today}
-                        ></Translate>{' '}
-                        (
-                        <span className="bold">
-                          +
-                          <Separator number={hosp[1].icu.in} /> -
-                          <Separator number={hosp[1].icu.out} />
-                        </span>
-                        ).
-                      </span>
-                      <br />
-                    </li>
-                  );
-                })}
-        </ul>
-      </span>
-
+      <PatientsLine
+        check_second={check_second}
+        patients={patients}
+        perHospitalChanges={perHospitalChanges}
+      />
       <MunicipalitiesLines
         check_third_mun={check_third_mun}
         municipalities={municipalities}
