a2a0564491a93eb9c403856bb4654d63901cf2cf
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-07 12:53:03 +0100

    refactor(getSledilnikApiPath): use new URL

--- src/helpers/getSledilnikApiPath.js
+++ src/helpers/getSledilnikApiPath.js
@@ -2,21 +2,17 @@ const BASE_API_URL = 'https://api.sledilnik.org/api';
 
 const getURLPath = baseURL => (path, params = {}) => {
   let apiPath = `${baseURL}/${path}`;
+  const url = new URL(apiPath);
   if (Object.keys(params).length === 0) {
-    return apiPath;
+    return url;
   }
 
-  apiPath += '?';
+  const pathParams = Object.entries(params);
+  for (const keyValuePair of pathParams) {
+    url.searchParams.append(...keyValuePair);
+  }
 
-  const apiPathWithParams = Object.entries(params)
-    .map(([key, value]) => {
-      return `${key}=${value}`;
-    })
-    .reduce((acc, curVal) => {
-      return `${acc}${curVal}&`;
-    }, apiPath);
-
-  return apiPathWithParams.slice(0, -1);
+  return url;
 };
 
 const getSledilnikApiPath = getURLPath(BASE_API_URL);
