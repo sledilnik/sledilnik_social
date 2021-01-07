40621b7827aec9a796db90fcb57c1bc33d52783b
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-07 15:50:18 +0100

    refactor(apiPathObject): move logic from apiSled..

--- src/utils/apiPathObject.js
+++ src/utils/apiPathObject.js
@@ -1,7 +1,9 @@
-import getSledilnikApiPath from '../helpers/getSledilnikApiPath';
 import { addDays } from 'date-fns';
 
 const dateFrom = addDays(new Date(), -18).toISOString();
+
+const BASE_API_URL = 'https://api.sledilnik.org';
+const baseURL = new URL(BASE_API_URL);
 
 const pathsParamsObject = {
   stats: { from: dateFrom },
@@ -14,12 +16,21 @@ const pathsParamsObject = {
 
 const pathsArray = Object.entries(pathsParamsObject);
 
+const replaceUnderscoreWithDash = val => val.replace('_', '-');
+
 const apiPathObject = (paths = []) => {
-  return paths.reduce((acc, curVal) => {
-    const key = `${curVal[0]}Path`;
-    const path = curVal[0].replace('_', '-');
-    const params = curVal[1];
-    acc[key] = getSledilnikApiPath(path, params);
+  return paths.reduce((acc, [_path, params]) => {
+    const key = `${_path}Path`;
+    const path = `api/${replaceUnderscoreWithDash(_path)}`;
+
+    const url = new URL(path, baseURL);
+
+    const pathParams = Object.entries(params);
+    for (const keyValuePair of pathParams) {
+      url.searchParams.append(...keyValuePair);
+    }
+
+    acc[key] = url;
     return acc;
   }, {});
 };
