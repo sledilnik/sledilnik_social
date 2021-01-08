f387b950a818b775e32f4bdd7843d3a3af34d6e7
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-08 11:25:26 +0100

    feat(ThirdTweet): embed first & second tweet

--- src/components/Tweets/ThirdTweet.js
+++ src/components/Tweets/ThirdTweet.js
@@ -3,12 +3,19 @@ import React from 'react';
 import PerAgeLine from '../PerAgeLine';
 import MunicipalitiesLines from '../MunicipalitiesLines';
 import PatientsLine from '../PatientsLine';
+import FirstTweet from './FirstTweet';
+import Arrow from '../Arrow';
+import Separator from '../Separator';
+import SecondTweet from './SecondTweet';
 
 function ThirdTweet({
+  check_first,
   check_second,
   check_third_age,
   check_third_mun,
   stats,
+  labTests,
+  summary,
   patients,
   municipalities,
   perHospitalChanges,
@@ -17,22 +24,39 @@ function ThirdTweet({
   const yesterdayPerAge = stats[stats.length - 3].statePerAgeToDate;
 
   return (
-    <div>
-      <PerAgeLine
-        check_third_age={check_third_age}
-        todayPerAge={todayPerAge}
-        yesterdayPerAge={yesterdayPerAge}
+    <>
+      <FirstTweet
+        check_first={check_first}
+        labTests={labTests}
+        summary={summary}
       />
-      <PatientsLine
+      <Arrow /> Skupaj{' '}
+      <span className="bold">
+        <Separator number={stats[stats.length - 2].cases.confirmedToDate} />
+      </span>{' '}
+      potrjenih primerov.
+      <SecondTweet
         check_second={check_second}
+        stats={stats}
         patients={patients}
-        perHospitalChanges={perHospitalChanges}
       />
-      <MunicipalitiesLines
-        check_third_mun={check_third_mun}
-        municipalities={municipalities}
-      />
-    </div>
+      <div>
+        <PerAgeLine
+          check_third_age={check_third_age}
+          todayPerAge={todayPerAge}
+          yesterdayPerAge={yesterdayPerAge}
+        />
+        <PatientsLine
+          check_second={check_second}
+          patients={patients}
+          perHospitalChanges={perHospitalChanges}
+        />
+        <MunicipalitiesLines
+          check_third_mun={check_third_mun}
+          municipalities={municipalities}
+        />
+      </div>
+    </>
   );
 }
 
