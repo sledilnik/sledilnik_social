82aa0bdf21097f1e04258810011067e8e21870c7
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-08 18:54:34 +0100

    feat(InOut): if numOut undefined no-show " -"

--- src/components/InOut.js
+++ src/components/InOut.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Separator from './Separator';
 
 function InOut({ numIn, numOut, insideColons = false }) {
+  const plus = numIn === undefined ? '' : '+';
+  const minus = numOut === undefined ? '' : '-';
+  const blank = numOut === undefined ? '' : ' ';
   return (
     <>
       {insideColons ? '(' : ''}
       <span className="bold">
-        +<Separator number={numIn} /> -<Separator number={numOut} />
+        {plus}
+        <Separator number={numIn} />
+        {blank}
+        {minus}
+        <Separator number={numOut} />
       </span>
       {insideColons ? ')' : ''}
     </>
