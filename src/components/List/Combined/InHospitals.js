c41fea1d2d1f174ab752556cfbf68bdd1fa8baf7
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-10 12:42:16 +0100

    refactor(InHospitals): desctrucutre

--- src/components/List/Combined/InHospitals.js
+++ src/components/List/Combined/InHospitals.js
@@ -5,44 +5,81 @@ import InOut from '../shared/InOut';
 import DataTranslate from '../shared/DataTranslate';
 
 function InHospitals({ check_second, patients, perHospitalChanges }) {
+  const DataWithInOut = ({ number, text, numIn, numOut }) => {
+    return (
+      <>
+        <DataTranslate number={number} text={text} />{' '}
+        <InOut numIn={numIn} numOut={numOut} insideColons={true} />
+      </>
+    );
+  };
+
+  const InHospital = ({
+    key,
+    hospitalName,
+    hosp = { number: 0, in: 0, out: 0 },
+    icu = { number: 0, in: 0, out: 0 },
+  }) => {
+    return (
+      <li key={key}>
+        <span className="bold">{hospitalName}</span>:{' '}
+        <DataWithInOut
+          number={hosp.number}
+          text={'oseba'}
+          numIn={hosp.in}
+          numOut={hosp.out}
+        />
+        , EIT{' '}
+        <DataWithInOut
+          number={icu.number}
+          text={'oseba'}
+          numIn={icu.in}
+          numOut={icu.out}
+        />
+        .
+      </li>
+    );
+  };
+
+  const sortDescByPatients = (a, b) =>
+    (b[1].inHospital.today || 0) - (a[1].inHospital.today || 0);
+
+  const createHospitalOutput = hosp => {
+    const hospital = {
+      number: hosp[1].inHospital.today,
+      in: hosp[1].inHospital.in,
+      out: hosp[1].inHospital.out,
+    };
+    const icu = {
+      number: hosp[1].icu.today,
+      in: hosp[1].icu.in,
+      out: hosp[1].icu.out,
+    };
+
+    const noHospitalData = hosp[1].inHospital.today === undefined;
+
+    return noHospitalData ? (
+      ''
+    ) : (
+      <InHospital
+        key={hosp[0]}
+        hospitalName={hosp[2]}
+        hosp={{ ...hospital }}
+        icu={{ ...icu }}
+      />
+    );
+  };
+
+  const noPatientsData = patients[patients.length - 1] === undefined;
+
+  const hospitalOutput = perHospitalChanges
+    .sort(sortDescByPatients)
+    .map(createHospitalOutput);
+
   return (
     <span className={check_second}>
       <DataRow title={'Stanje po bolnišnicah'} />
-      <ul>
-        {patients[patients.length - 1] === undefined
-          ? 'NI PODATKOV'
-          : perHospitalChanges
-              .sort(
-                (a, b) =>
-                  (b[1].inHospital.today || 0) - (a[1].inHospital.today || 0)
-              )
-              .map(hosp => {
-                return hosp[1].inHospital.today === undefined ? (
-                  ''
-                ) : (
-                  <li key={hosp[0]}>
-                    <span className="bold">{hosp[2]}</span>:{' '}
-                    <DataTranslate
-                      number={hosp[1].inHospital.today}
-                      text={'oseba'}
-                    />{' '}
-                    <InOut
-                      numIn={hosp[1].inHospital.in}
-                      numOut={hosp[1].inHospital.out}
-                      insideColons={true}
-                    />
-                    , EIT{' '}
-                    <DataTranslate number={hosp[1].icu.today} text={'oseba'} />{' '}
-                    <InOut
-                      numIn={hosp[1].icu.in}
-                      numOut={hosp[1].icu.out}
-                      insideColons={true}
-                    />
-                    .
-                  </li>
-                );
-              })}
-      </ul>
+      <ul>{noPatientsData ? 'NI PODATKOV' : hospitalOutput}</ul>
     </span>
   );
 }
