db3662c4c50031ac642273fd212b42382e57ab82
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-10 16:23:44 +0100

    feat(components): add DataWithRatio

---
+++ src/components/List/shared/DataWithRatio.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import StyledNumberWithText from './StyledNumberWithText';
+import Percentage from '../TESTS_ACTIVE/Percentage';
+
+function DataWithRatio({ numPositive, numPerformed }) {
+  return (
+    <>
+      <StyledNumberWithText
+        className="bold"
+        number={numPositive}
+        numSign={true}
+        suffix=","
+      />
+      <StyledNumberWithText
+        className="bold"
+        number={numPerformed}
+        prefix={'testiranih: '}
+        suffix={', delež pozitivnih: '}
+      />
+      <Percentage part={numPositive} total={numPerformed}></Percentage>
+      %.
+    </>
+  );
+}
+
+export default DataWithRatio;
