ec6d9e6f9556a564e2dac8137eb22d8b8417bdf8
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-10 15:23:15 +0100

    feat(components): add DataWithInOut

---
+++ src/components/List/shared/DataWithInOut.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import StyledNumberWithText from './StyledNumberWithText';
+import InOut from './InOut';
+
+function DataWithInOut({
+  number,
+  prefix,
+  suffix,
+  numSign = false,
+  numIn,
+  numOut,
+  insideColons,
+}) {
+  return (
+    <>
+      <StyledNumberWithText
+        number={number}
+        prefix={prefix}
+        suffix={suffix}
+        numSign={numSign}
+      />
+      <InOut numIn={numIn} numOut={numOut} insideColons={insideColons} />
+    </>
+  );
+}
+
+export default DataWithInOut;
