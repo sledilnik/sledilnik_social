88dc2357625760680608505c363fe9b2ca33dd89
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-10 04:11:33 +0100

    refactor(HOSP_DECE): each DataRow -> function

--- src/components/shared/HOSPITALIZED_DECEASED.js
+++ src/components/shared/HOSPITALIZED_DECEASED.js
@@ -7,64 +7,90 @@ import InOut from '../List/shared/InOut';
 import DataTranslate from '../List/shared/DataTranslate';
 
 function HOSPITALIZED_DECEASED({ check_second, stats, patients }) {
-  return (
-    <div className={check_second}>
-      <DataRow title={'Hospitalizirani'}>
-        <DataTranslate
-          number={stats[stats.length - 1].statePerTreatment.inHospital}
-          text={'oseba'}
-        />
-        <InOut
-          numIn={patients[patients.length - 1].total.inHospital.in}
-          numOut={patients[patients.length - 1].total.inHospital.out}
-          insideColons={true}
-        />
-        , v EIT{' '}
-        <DataTranslate
-          number={stats[stats.length - 1].statePerTreatment.inICU}
-          text={'oseba'}
-        />
+  const hospNum = stats[stats.length - 1].statePerTreatment.inHospital;
+  const hospIn = patients[patients.length - 1].total.inHospital.in;
+  const hospOut = patients[patients.length - 1].total.inHospital.out;
+  const icuNum = stats[stats.length - 1].statePerTreatment.inICU;
+  const todayICU = stats[stats.length - 1].statePerTreatment.inICU;
+  const yesterdayICU = stats[stats.length - 2].statePerTreatment.inICU;
+
+  const todayCritical = stats[stats.length - 1].statePerTreatment.critical;
+  const yesterdayCritical = stats[stats.length - 2].statePerTreatment.critical;
+
+  const { deceased, deceasedToDate } = stats[
+    stats.length - 1
+  ].statePerTreatment;
+
+  function Hospitalized({
+    title,
+    subtitle,
+    totalNum,
+    inOut = [0, 0],
+    icuNum,
+    icuDelta = [0, 0],
+  }) {
+    return (
+      <DataRow title={title}>
+        <DataTranslate number={totalNum} text={'oseba'} />
+        <InOut numIn={inOut[0]} numOut={inOut[1]} insideColons={true} />,{' '}
+        {subtitle} <DataTranslate number={icuNum} text={'oseba'} />
         <Delta
-          today={stats[stats.length - 1].statePerTreatment.inICU}
-          yesterday={stats[stats.length - 2].statePerTreatment.inICU}
+          today={icuDelta[0]}
+          yesterday={icuDelta[1]}
           insideColons={true}
           getPrefix={true}
           noChanges={true}
         ></Delta>
         .
       </DataRow>
+    );
+  }
+
+  function OnRespiratory({ today, yesterday }) {
+    return (
+      // TODO DataRow if noColon true add space instead
       <DataRow title={'Na respiratorju (intubirani) se'} noColon={true}>
-        <Translate
-          text={'zdravi'}
-          number={stats[stats.length - 1].statePerTreatment.critical}
-        ></Translate>{' '}
-        <DataTranslate
-          number={stats[stats.length - 1].statePerTreatment.critical}
-          text={'oseba'}
-        />{' '}
+        {' '}
+        <Translate text={'zdravi'} number={today}></Translate>{' '}
+        <DataTranslate number={today} text={'oseba'} />{' '}
         <Delta
-          today={stats[stats.length - 1].statePerTreatment.critical}
-          yesterday={stats[stats.length - 2].statePerTreatment.critical}
+          today={today}
+          yesterday={yesterday}
           insideColons={true}
           getPrefix={true}
           noChanges={true}
         ></Delta>
         .
       </DataRow>
+    );
+  }
+
+  function Deceased({ deceased, deceasedToDate }) {
+    const plusOrEmpty = deceased > 0 ? '+' : '';
+    return (
       <DataRow title={'Preminuli'}>
-        {stats[stats.length - 1].statePerTreatment.deceased > 0 ? '+' : ''}
-        <DataTranslate
-          number={stats[stats.length - 1].statePerTreatment.deceased}
-          text={'oseba'}
-        />
-        , skupaj:{' '}
+        {plusOrEmpty}
+        <DataTranslate number={deceased} text={'oseba'} />, skupaj:{' '}
         <span className="bold">
-          <Separator
-            number={stats[stats.length - 1].statePerTreatment.deceasedToDate}
-          />
+          <Separator number={deceasedToDate} />
         </span>
         .
       </DataRow>
+    );
+  }
+
+  return (
+    <div className={check_second}>
+      <Hospitalized
+        title={'Hospitalizirani'}
+        subtitle={'v EIT'}
+        totalNum={hospNum}
+        inOut={[hospIn, hospOut]}
+        icuNum={icuNum}
+        icuDelta={[todayICU, yesterdayICU]}
+      />
+      <OnRespiratory today={todayCritical} yesterday={yesterdayCritical} />
+      <Deceased deceased={deceased} deceasedToDate={deceasedToDate} />
     </div>
   );
 }
