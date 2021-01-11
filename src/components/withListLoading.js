908205e8eaf0e331d72ab8d5914c22002e08d174
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-11 20:20:46 +0100

    feat(withListLoading): use Modal and Backdrop

--- src/components/withListLoading.js
+++ src/components/withListLoading.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Loader from './Loader';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
 
 function WithListLoading(Component) {
   return function WihLoadingComponent({ isLoading, ...props }) {
     if (!isLoading) return <Component {...props} />;
-    return <Loader />;
+    return (
+      <Modal>
+        <Backdrop>
+          <Loader />
+        </Backdrop>
+      </Modal>
+    );
   };
 }
 export default WithListLoading;
