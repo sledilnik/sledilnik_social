dd88364e6860b674301a5a34d0930767b4e7dbd5
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-11 03:40:09 +0100

    fix(StyledNumber): no need to add minus sign
    if number is negative minus sign is already included

--- src/components/List/shared/StyledNumber.js
+++ src/components/List/shared/StyledNumber.js
@@ -3,15 +3,11 @@ import Separator from './Separator';
 
 function StyledNumber({ number, preSign = false, className }) {
   const isPositive = number > 0;
-  const isNegative = number < 0;
-
   const signShouldBePlus = preSign && isPositive;
-  const signShouldBeMinus = preSign && isNegative;
 
   return (
     <span className={`${className}`}>
       {signShouldBePlus && '+'}
-      {signShouldBeMinus && '-'}
       <Separator number={number} />
     </span>
   );
