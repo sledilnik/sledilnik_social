3ee54cb1feb2c5706d2b7c81f02d2d26f3c2c3fe
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-13 12:11:30 +0100

    fix(Intro): best practice

--- src/components/List/Intro.js
+++ src/components/List/Intro.js
@@ -1,15 +1,15 @@
-import React from "react";
-const Intro = (props) => {
+import React from 'react';
+const Intro = props => {
   return (
-    <p className="text">
+    <h2 className="text">
       <span className="bold">
-        Status #COVID19{" "}
+        Status #COVID19{' '}
         <span role="img" aria-label="s">
           🇸🇮
-        </span>{" "}
+        </span>{' '}
         {props.post}/3 {props.introTodayDate}
       </span>
-    </p>
+    </h2>
   );
 };
 export default Intro;
