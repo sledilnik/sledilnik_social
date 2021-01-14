2d9a502ccd116ea815f5be947fab9edde5222bd0
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-14 13:13:58 +0100

    fix(Percentage): should not play with git stash

--- src/components/List/TESTS_ACTIVE/Percentage.js
+++ src/components/List/TESTS_ACTIVE/Percentage.js
@@ -21,20 +21,3 @@ const Percentage = ({
   );
 };
 export default Percentage;
-
-  const percentage =
-    denominator &&
-    numerator &&
-    Math.round(
-      (numerator / denominator - (minus1 === true ? 1 : 0) + Number.EPSILON) *
-        1000
-    ) / 10;
-
-  return (
-    <span className="bold">
-      {getPrefix ? (numerator > denominator ? '+' : '-') : ''}
-      {percentage.toString().replace(/\./g, ',')}
-    </span>
-  );
-};
-export default Percentage;
