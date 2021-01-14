dab96e3a1abb56820c1bc9290b7327af918af0eb
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-14 13:07:22 +0100

    feat(Percentage): add defaults to props

--- src/components/List/TESTS_ACTIVE/Percentage.js
+++ src/components/List/TESTS_ACTIVE/Percentage.js
@@ -1,5 +1,27 @@
 import React from 'react';
-const Percentage = ({ numerator, denominator, minus1, getPrefix }) => {
+const Percentage = ({
+  numerator = 0,
+  denominator = 0,
+  minus1 = false,
+  getPrefix = false,
+}) => {
+  const percentage =
+    denominator &&
+    numerator &&
+    Math.round(
+      (numerator / denominator - (minus1 === true ? 1 : 0) + Number.EPSILON) *
+        1000
+    ) / 10;
+
+  return (
+    <span className="bold">
+      {getPrefix ? (numerator > denominator ? '+' : '-') : ''}
+      {percentage.toLocaleString('sl-SL')}
+    </span>
+  );
+};
+export default Percentage;
+
   const percentage =
     denominator &&
     numerator &&
