d05b4b0f3df5d1348c31adea7f745f2739fab7a6
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-14 17:43:51 +0100

    feat(DataRow): add option to add punctuation mark

--- src/components/List/shared/DataRow.js
+++ src/components/List/shared/DataRow.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Arrow from './Arrow';
 
-function DataRow({ children, title, noColon }) {
+function DataRow({
+  children,
+  title,
+  noColon,
+  endOfSentence = { end: true, punctuationMark: '.' },
+}) {
   let colon = !title || noColon ? ' ' : ': ';
+
+  const { end, punctuationMark } = endOfSentence;
 
   return (
     <p className="text">
       <Arrow /> {title}
       {colon}
       {children}
+      {end && punctuationMark}
     </p>
   );
 }
