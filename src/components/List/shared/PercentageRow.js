5ad262a5de90b46480a213add0da283d10ebf88f
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-15 12:02:50 +0100

    feat(PercentageRow): move logic from <Percentage>

--- src/components/List/shared/PercentageRow.js
+++ src/components/List/shared/PercentageRow.js
@@ -9,16 +9,36 @@ const defaults = {
   getPrefix: false,
 };
 
+const getFraction = (numerator = 0, denominator = 0) => numerator / denominator;
+const getPercentage = value => value * 100;
+
 function PercentageRow({
   title = '',
   numeratorOptions = { ...defaults },
   denominatorOptions = { ...defaults },
 }) {
+  const { number: numerator } = numeratorOptions;
+  const { number: denominator } = denominatorOptions;
+  const canNotCalc = isNaN(numerator) || isNaN(denominator);
+
+  if (canNotCalc) {
+    return (
+      <DataRow title={title} endOfSentence={{ punctuationMark: '!' }}>
+        <span>{' - odstotka ni mogoče izračunati'}</span>
+      </DataRow>
+    );
+  }
+
+  const fraction = getFraction(numerator, denominator);
+
+  const percentage = getPercentage(fraction);
+
   return (
     <DataRow title={title}>
       <DataPercentage
         numeratorOptions={numeratorOptions}
         denominatorOptions={denominatorOptions}
+        percentage={percentage}
       />
     </DataRow>
   );
