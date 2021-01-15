bfde7a18408954262734fc9e72609a7c247861b5
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-15 10:21:01 +0100

    feat(StlyedNumber): check for null/undefined &
    add prop fractionDigits
    add default value for prop className
    
    refactor:
    preSign -> getPrefix
    <Separator> -> use toLocaleString with parseFloat.toFixed

--- src/components/List/shared/StyledNumber.js
+++ src/components/List/shared/StyledNumber.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import Separator from './Separator';
 
-function StyledNumber({ number, preSign = false, className }) {
+const isUndefined = value => value === undefined;
+const isNull = value => value === null;
+const isNullOrUndefined = value => isNull(value) || isUndefined(value);
+
+const toLocale = value => value.toLocaleString('sl-SL');
+
+function StyledNumber({
+  number,
+  fractionDigits = 0,
+  getPrefix = false,
+  className = '',
+}) {
+  if (isNullOrUndefined(number)) {
+    return <span className={className}>{' - '}</span>;
+  }
+
   const isPositive = number > 0;
-  const signShouldBePlus = preSign && isPositive;
+  const signShouldBePlus = getPrefix && isPositive;
+  const prefix = signShouldBePlus && '+';
+
+  const rounded = +parseFloat(number).toFixed(fractionDigits);
+  const locale = toLocale(rounded);
 
   return (
-    <span className={`${className}`}>
-      {signShouldBePlus && '+'}
-      <Separator number={number} />
+    <span className={className}>
+      {prefix}
+      {locale}
     </span>
   );
 }
