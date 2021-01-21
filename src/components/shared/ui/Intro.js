3c86b941814e33b563a433d57f3495d26442bcdc
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-21 15:40:35 +0100

    feature(Intro): introduce var for posts' count

--- src/components/shared/ui/Intro.js
+++ src/components/shared/ui/Intro.js
@@ -3,12 +3,19 @@ import React from 'react';
 import './Intro.css';
 import { Emoji } from './New';
 
+const mainComponentsNames = [
+  'TESTS_ACTIVE',
+  'HOSPITALIZED_DECEASED',
+  'Combined',
+];
+
 const Intro = ({ post, introTodayDate }) => {
+  const postsCount = mainComponentsNames.length;
   return (
     <h2 className="Intro text">
       <span className="bold">
-        Status #COVID19 <Emoji emoji={'🇸🇮'} ariaLabel={'flag'} /> {post}/3{' '}
-        {introTodayDate}
+        Status #COVID19 <Emoji emoji={'🇸🇮'} ariaLabel={'flag'} /> {post}/
+        {postsCount} {introTodayDate}
       </span>
     </h2>
   );
