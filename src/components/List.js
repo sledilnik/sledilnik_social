d7bec1d6e8512903766140b56b86511ae2975b1b
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-26 16:31:02 +0100

    feat(List): introduce remove consecutive \n

--- src/components/List.js
+++ src/components/List.js
@@ -8,7 +8,6 @@ import TESTS_ACTIVE from './List/TESTS_ACTIVE';
 import HOSPITALIZED_DECEASED from './List/HOSPITALIZED_DECEASED';
 import Combined from './List/Combined';
 
-
 import { formatToLocaleDateString } from '../utils/dates';
 
 const List = ({
@@ -21,8 +20,30 @@ const List = ({
 }) => {
   const introTodayDate = formatToLocaleDateString(new Date(), 'd.M.yyyy');
 
+  const clickHandler = () => {
+    /* Get the text field */
+    const copyText = document.getElementById('copy');
+    copyText.value = copyText.value.replace(/(\r\n|\r|\n){2,}/g, '\n');
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    document.execCommand('copy');
+  };
+
   return (
     <div className="List">
+      <div style={{ display: 'block' }}>
+        <label htmlFor="copy" id="copy-label">
+          Remove consecutive new lines & Copy
+        </label>
+        <div className="textwrapper">
+          <textarea cols="2" rows="10" id="copy" />
+        </div>
+        <button onClick={clickHandler}>OK</button>
+      </div>
       <section className="post">
         <Intro post={1} introTodayDate={introTodayDate} />
         <TESTS_ACTIVE labTestsHook={labTestsHook} summaryHook={summaryHook} />
