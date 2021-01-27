4a3fcd853f935c5ba81f7429c50936d3c5bb4d4a
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-27 09:53:15 +0100

    refactor(List): +TrimNewLines

--- src/components/List.js
+++ src/components/List.js
@@ -10,17 +10,8 @@ import Combined from './List/Combined';
 
 import { formatToLocaleDateString } from '../utils/dates';
 
-const List = ({
-  statsHook,
-  patientsHook,
-  municipalitiesHook,
-  hospitalsListHook,
-  labTestsHook,
-  summaryHook,
-}) => {
+const TrimNewLines = () => {
   const [length, setLength] = useState(0);
-  const introTodayDate = formatToLocaleDateString(new Date(), 'd.M.yyyy');
-
   const clickHandler = () => {
     const copyText = document.getElementById('copy');
     copyText.value = copyText.value.replace(/(\r\n|\r|\n){2,}/g, '\n');
@@ -36,28 +27,43 @@ const List = ({
   };
 
   return (
-    <div className="List">
-      <div style={{ display: 'block', fontSize: '0.9em' }}>
-        <label htmlFor="copy" id="copy-label">
-          Odstrani odvečne vrstice, označi in prenese v odložišče.
-        </label>{' '}
-        <p style={{ margin: '0.5em 0' }}>Število znakov: {length}</p>
-        <div className="textwrapper">
-          <textarea cols="50" rows="10" id="copy" onChange={changeHandler} />
-        </div>
-        <div className="text-area-btn-container">
-          <button className="btn" onClick={clickHandler}>
-            V odložišče
-          </button>
-          <a
-            href="https://twitter.com/intent/tweet?button_hashtag=COVID19&ref_src=twsrc%5Etfw"
-            className="twitter-hashtag-button"
-            data-show-count="false"
-          >
-            Tweet #COVID19
-          </a>
-        </div>
+    <div style={{ display: 'block', fontSize: '0.9em' }}>
+      <label htmlFor="copy" id="copy-label">
+        Odstrani odvečne vrstice, označi in prenese v odložišče.
+      </label>{' '}
+      <p style={{ margin: '0.5em 0' }}>Število znakov: {length}</p>
+      <div className="textwrapper">
+        <textarea cols="50" rows="10" id="copy" onChange={changeHandler} />
       </div>
+      <div className="text-area-btn-container">
+        <button className="btn" onClick={clickHandler}>
+          V odložišče
+        </button>
+        <a
+          href="https://twitter.com/intent/tweet?button_hashtag=COVID19&ref_src=twsrc%5Etfw"
+          className="twitter-hashtag-button"
+          data-show-count="false"
+        >
+          Tweet #COVID19
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const List = ({
+  statsHook,
+  patientsHook,
+  municipalitiesHook,
+  hospitalsListHook,
+  labTestsHook,
+  summaryHook,
+}) => {
+  const introTodayDate = formatToLocaleDateString(new Date(), 'd.M.yyyy');
+
+  return (
+    <div className="List">
+      <TrimNewLines />
       <section className="post">
         <Intro post={1} introTodayDate={introTodayDate} />
         <TESTS_ACTIVE labTestsHook={labTestsHook} summaryHook={summaryHook} />
