aa9fcfb1934069b6540156f985888079489c56e9
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-29 08:19:28 +0100

    ferat(App): introduce Refresh

--- src/components/List.js
+++ src/components/List.js
@@ -80,20 +80,45 @@ const List = ({
 }) => {
   const introTodayDate = formatToLocaleDateString(new Date(), 'd.M.yyyy');
 
+  const Refresh = ({ refetch = [] }) => {
+    const clickHandler = () => {
+      refetch.forEach(func => func());
+    };
+    return (
+      <div className="refetch-container">
+        <button className="btn" onClick={clickHandler}>
+          Osveži
+        </button>
+      </div>
+    );
+  };
+
   return (
     <div className="List">
       <TrimNewLines />
       <section className="post">
+        <Refresh refetch={[labTestsHook.refetch, summaryHook.refetch]} />
         <Intro post={1} introTodayDate={introTodayDate} />
         <TESTS_ACTIVE labTestsHook={labTestsHook} summaryHook={summaryHook} />
         <Outro />
       </section>
       <section className="post">
+        <Refresh refetch={[patientsHook.refetch]} />
         <Intro post={2} introTodayDate={introTodayDate} />
         <HOSPITALIZED_DECEASED patientsHook={patientsHook} />
         <Outro />
       </section>
       <section className="post">
+        <Refresh
+          refetch={[
+            labTestsHook.refetch,
+            summaryHook.refetch,
+            patientsHook.refetch,
+            statsHook.refetch,
+            municipalitiesHook.refetch,
+            hospitalsListHook.refetch,
+          ]}
+        />
         <Intro post={3} introTodayDate={introTodayDate} />
         <Combined
           statsHook={statsHook}
