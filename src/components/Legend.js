71d4cbf39efa59ab23ebfa5bb89233fa25d9f74d
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-01-30 12:35:08 +0100

    refactor(PathsDates): move logic from Legend

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import differenceInDays from 'date-fns/differenceInDays';
 
 import './Legend.css';
-import apiPathObject from '../utils/apiPathObject';
-import { getDate, formatToLocaleDateString } from '../utils/dates';
 import RatioTrends from './Legend/RatioTrends';
 import PathsDates from './Legend/PathsDates';
 
@@ -14,16 +11,11 @@ export const LegendSection = ({ title, children }) => (
   </section>
 );
 
-function Legend({ pathsDatesData, municipalitiesHook }) {
+function Legend({ pathsDatesHooks, municipalitiesHook }) {
   return (
     <div className="Legend">
       <h2 id="legenda">Legenda</h2>
-      <PathsDates
-        css={pathsDatesData?.css}
-        dates={pathsDatesData?.dates}
-        paths={pathsDatesData?.paths}
-        refreshData={pathsDatesData?.refreshData}
-      />
+      <PathsDates hooks={pathsDatesHooks} />
       <RatioTrends municipalitiesHook={municipalitiesHook} />
     </div>
   );
@@ -31,35 +23,8 @@ function Legend({ pathsDatesData, municipalitiesHook }) {
 
 function withLegendHOC(Component) {
   return ({ ...props }) => {
-    const stats = props.statsHook.data;
-    const patients = props.patientsHook.data;
-    const municipalities = props.municipalitiesHook.data;
-    // const hospitalsList = props.hospitalsListHook.data;
-    const labTests = props.labTestsHook.data;
-    const summary = props.summaryHook.data;
-
-    const isLoading =
-      props.statsHook.isLoading ||
-      props.patientsHook.isLoading ||
-      props.municipalitiesHook.isLoading ||
-      props.labTestsHook.isLoading ||
-      props.summaryHook.isLoading;
-
-    const allDataExists =
-      !isLoading &&
-      stats !== null &&
-      patients !== null &&
-      municipalities !== null &&
-      labTests !== null &&
-      summary !== null;
-
-    // ? move logic to <PathsDates/>
-    const pathsDatesData =
-      allDataExists &&
-      getPathsDatesData({ stats, patients, municipalities, labTests, summary });
-
     const legendProps = {
-      pathsDatesData,
+      pathsDatesHooks: { ...props },
       municipalitiesHook: props.municipalitiesHook,
     };
 
@@ -68,131 +33,3 @@ function withLegendHOC(Component) {
 }
 
 export default withLegendHOC(Legend);
-
-function getPathsDatesData(
-  allData = {
-    stats: [],
-    patients: [],
-    municipalities: [],
-    labTests: [],
-    summary: {},
-  }
-) {
-  const dates = getDates(allData);
-
-  const cssChecks = getChecks({ ...allData });
-
-  const css = {
-    stats: cssChecks.check_stats,
-    patients: cssChecks.check_patients,
-    labTests: cssChecks.check_lab_tests,
-    municipalities: cssChecks.check_municipalities,
-    summary: cssChecks.check_summary,
-  };
-
-  const paths = {
-    stats: apiPathObject.statsPath,
-    patients: apiPathObject.patientsPath,
-    labTests: apiPathObject.lab_testsPath,
-    municipalities: apiPathObject.municipalitiesPath,
-    summary: apiPathObject.summaryPath,
-  };
-
-  // TODO find better name
-  const refreshData =
-    dates & css &&
-    paths &&
-    [css, dates, paths].reduce((acc, obj) => {
-      for (const [key, value] of Object.entries(obj)) {
-        acc[key] = acc[key] ? [...acc[key], value] : [value];
-      }
-      return acc;
-    }, {});
-
-  return {
-    dates,
-    css,
-    paths,
-    refreshData,
-    municipalities: allData.municipalities,
-  };
-}
-
-function getDates(allData = {}) {
-  const { stats, patients, municipalities, labTests, summary } = allData;
-
-  const data = getLastUpdatedData({
-    stats,
-    patients,
-    municipalities,
-    labTests,
-  });
-
-  const statsDate = getDate(data.statsData);
-  const patientsDate = getDate(data.patientsData);
-  const municipalitiesDate = getDate(data.municipalitiesData);
-  const labTestsDate = getDate(data.labTestsData);
-  const summaryDate = getDate(summary.casesActive); // change here, change in List.getChecks
-
-  const dates = {
-    today: formatToLocaleDateString(new Date(), 'd.M.yyyy'),
-    stats: formatToLocaleDateString(statsDate, 'd.M.yyyy'),
-    patients: formatToLocaleDateString(patientsDate, 'd.M.yyyy'),
-    municipalities: formatToLocaleDateString(municipalitiesDate, 'd.M.yyyy'),
-    labTests: formatToLocaleDateString(labTestsDate, 'd.M.yyyy'),
-    summary: formatToLocaleDateString(summaryDate, 'd.M.yyyy'),
-  };
-
-  return dates;
-}
-
-function getLastUpdatedData({ stats, municipalities, patients, labTests }) {
-  return {
-    patientsData: patients.slice(-1).pop(),
-    statsData: stats.slice(-1).pop(),
-    municipalitiesData: municipalities.slice(-1).pop(),
-    labTestsData: labTests.slice(-1).pop(),
-  };
-}
-
-function getChecks({ stats, municipalities, patients, summary, labTests }) {
-  const isUndefined = val => val === undefined;
-  // data
-  const lastUpdatedData = getLastUpdatedData({
-    stats,
-    patients,
-    municipalities,
-    labTests,
-  });
-
-  // dates
-  const patientsDate = getDate(lastUpdatedData.patientsData);
-  const statsDate = getDate(lastUpdatedData.statsData);
-  const municipalitiesDate = getDate(lastUpdatedData.municipalitiesData);
-  const labTestsDate = getDate(lastUpdatedData.labTestsData);
-  const summaryDate = getDate(summary.casesActive); // before labTests
-
-  // checks
-  const patientsCheck = differenceInDays(new Date(), patientsDate) > 0;
-
-  const isPerAgeDataUndefined = isUndefined(
-    stats.slice(-2, -1).pop().statePerAgeToDate[0].allToDate
-  );
-  const statsCheck =
-    isPerAgeDataUndefined || differenceInDays(new Date(), statsDate) > 0;
-
-  const municipalitiesCheck =
-    differenceInDays(new Date(), municipalitiesDate) > 1;
-
-  const labTestsCheck = differenceInDays(new Date(), labTestsDate) > 1;
-
-  const summaryCheck = differenceInDays(new Date(), summaryDate) > 1;
-
-  return {
-    check_summary: summaryCheck ? 'red' : '',
-    check_patients: patientsCheck ? 'red' : '',
-    check_stats: statsCheck ? 'red' : '',
-    check_municipalities: municipalitiesCheck ? 'red' : '',
-    check_lab_tests: labTestsCheck ? 'red' : '',
-  };
-}
--- src/components/Legend/PathsDates.js
+++ src/components/Legend/PathsDates.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Emoji, Arrow } from '../shared/ui/New';
+import differenceInDays from 'date-fns/differenceInDays';
+import { Emoji, Arrow, RowSkeleton, RowError } from '../shared/ui/New';
 import { LegendSection } from '../Legend';
+import { getDate, formatToLocaleDateString } from '../../utils/dates';
+import apiPathObject from '../../utils/apiPathObject';
 
 function PathsDates({ css = {}, dates = {}, paths = {}, refreshData = {} }) {
   const apiDates = Object.entries(refreshData).map(([key, value], index) => {
@@ -86,12 +89,168 @@ function PathsDates({ css = {}, dates = {}, paths = {}, refreshData = {} }) {
 
 function withPathsDatesHOC(Component) {
   return ({ ...props }) => {
-    // ? find better solution
-    if (props.css === undefined) {
-      return null;
+    const { hooks } = props;
+    const hooksObj = Object.entries(hooks).reduce(
+      (acc, [hookName, hook]) => {
+        const key = hookName.replace('Hook', '');
+        acc.isLoading[key] = hook.isLoading;
+        acc.data[key] = hook.data;
+        acc.errors[key] = hook.hasError;
+        return acc;
+      },
+      { isLoading: {}, data: {}, errors: {} }
+    );
+
+    const isLoading = Object.values(hooksObj.isLoading).some(item => item);
+    const hasError = Object.values(hooksObj.errors).some(item => item);
+    const allDataExists = Object.values(hooksObj.data).every(
+      item => item !== null
+    );
+
+    if (!isLoading && !hasError && !allDataExists) {
+      return <RowSkeleton />;
     }
 
-    return <Component {...props} />;
+    if (isLoading) {
+      return <RowSkeleton />;
+    }
+
+    if (hasError) {
+      return <RowError />;
+    }
+
+    const pathsDatesData =
+      allDataExists && getPathsDatesData({ ...hooksObj.data });
+
+    return <Component {...pathsDatesData} />;
   };
 }
 export default withPathsDatesHOC(PathsDates);
+
+function getPathsDatesData(
+  allData = {
+    stats: [],
+    patients: [],
+    municipalities: [],
+    labTests: [],
+    summary: {},
+  }
+) {
+  const dates = getDates(allData);
+
+  const cssChecks = getChecks({ ...allData });
+
+  const css = {
+    stats: cssChecks.check_stats,
+    patients: cssChecks.check_patients,
+    labTests: cssChecks.check_lab_tests,
+    municipalities: cssChecks.check_municipalities,
+    summary: cssChecks.check_summary,
+  };
+
+  const paths = {
+    stats: apiPathObject.statsPath,
+    patients: apiPathObject.patientsPath,
+    labTests: apiPathObject.lab_testsPath,
+    municipalities: apiPathObject.municipalitiesPath,
+    summary: apiPathObject.summaryPath,
+  };
+
+  // TODO find better name
+  const refreshData =
+    dates & css &&
+    paths &&
+    [css, dates, paths].reduce((acc, obj) => {
+      for (const [key, value] of Object.entries(obj)) {
+        acc[key] = acc[key] ? [...acc[key], value] : [value];
+      }
+      return acc;
+    }, {});
+
+  return {
+    dates,
+    css,
+    paths,
+    refreshData,
+    municipalities: allData.municipalities,
+  };
+}
+
+function getDates(allData = {}) {
+  const { stats, patients, municipalities, labTests, summary } = allData;
+
+  const data = getLastUpdatedData({
+    stats,
+    patients,
+    municipalities,
+    labTests,
+  });
+
+  const statsDate = getDate(data.statsData);
+  const patientsDate = getDate(data.patientsData);
+  const municipalitiesDate = getDate(data.municipalitiesData);
+  const labTestsDate = getDate(data.labTestsData);
+  const summaryDate = getDate(summary.casesActive); // change here, change in List.getChecks
+
+  const dates = {
+    today: formatToLocaleDateString(new Date(), 'd.M.yyyy'),
+    stats: formatToLocaleDateString(statsDate, 'd.M.yyyy'),
+    patients: formatToLocaleDateString(patientsDate, 'd.M.yyyy'),
+    municipalities: formatToLocaleDateString(municipalitiesDate, 'd.M.yyyy'),
+    labTests: formatToLocaleDateString(labTestsDate, 'd.M.yyyy'),
+    summary: formatToLocaleDateString(summaryDate, 'd.M.yyyy'),
+  };
+
+  return dates;
+}
+
+function getLastUpdatedData({ stats, municipalities, patients, labTests }) {
+  return {
+    patientsData: patients.slice(-1).pop(),
+    statsData: stats.slice(-1).pop(),
+    municipalitiesData: municipalities.slice(-1).pop(),
+    labTestsData: labTests.slice(-1).pop(),
+  };
+}
+
+function getChecks({ stats, municipalities, patients, summary, labTests }) {
+  const isUndefined = val => val === undefined;
+  // data
+  const lastUpdatedData = getLastUpdatedData({
+    stats,
+    patients,
+    municipalities,
+    labTests,
+  });
+
+  // dates
+  const patientsDate = getDate(lastUpdatedData.patientsData);
+  const statsDate = getDate(lastUpdatedData.statsData);
+  const municipalitiesDate = getDate(lastUpdatedData.municipalitiesData);
+  const labTestsDate = getDate(lastUpdatedData.labTestsData);
+  const summaryDate = getDate(summary.casesActive); // before labTests
+
+  // checks
+  const patientsCheck = differenceInDays(new Date(), patientsDate) > 0;
+
+  const isPerAgeDataUndefined = isUndefined(
+    stats.slice(-2, -1).pop().statePerAgeToDate[0].allToDate
+  );
+  const statsCheck =
+    isPerAgeDataUndefined || differenceInDays(new Date(), statsDate) > 0;
+
+  const municipalitiesCheck =
+    differenceInDays(new Date(), municipalitiesDate) > 1;
+
+  const labTestsCheck = differenceInDays(new Date(), labTestsDate) > 1;
+
+  const summaryCheck = differenceInDays(new Date(), summaryDate) > 1;
+
+  return {
+    check_summary: summaryCheck ? 'red' : '',
+    check_patients: patientsCheck ? 'red' : '',
+    check_stats: statsCheck ? 'red' : '',
+    check_municipalities: municipalitiesCheck ? 'red' : '',
+    check_lab_tests: labTestsCheck ? 'red' : '',
+  };
+}
