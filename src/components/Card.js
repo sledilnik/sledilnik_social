1a6752a537ad637fb34caa5b68294770c6c770c1
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-11 19:13:30 +0100

    feat(Card): make summary h2

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Card({ summary, children, open = false }) {
   return (
     <details open={open}>
-      <summary>{summary}</summary>
+      <summary>
+        <h2>{summary}</h2>
+      </summary>
       {children}
     </details>
   );
