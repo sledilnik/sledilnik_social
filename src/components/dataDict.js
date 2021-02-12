04730787545941509c92ab408b67285d6fd97907
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-12 16:24:34 +0100

    feat(dataDict): +inHospitals

--- src/components/dataDict.js
+++ src/components/dataDict.js
@@ -172,6 +172,7 @@ export const patients = {
       suffix: '.',
     },
   ],
+  inHospitals: [{ prefix: 'Stanje po bolnišnicah:' }],
 };
 
 const perAge = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((acc, item) => {
