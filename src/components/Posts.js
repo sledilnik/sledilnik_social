baba9ffcb76a54f9c4a8ba5f3524b5b286f427ee
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-12 15:14:31 +0100

    feat(Posts): extract each Card to show timestamps

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from './Card';
 import Post from './Post';
@@ -12,38 +12,74 @@ import OnRespiratory from './OnRespiratory';
 import Care from './Care';
 import Deceased from './Deceased';
 
+import ComfirmedToDate from './ConfirmedToDate';
+import Vaccination from './Vaccination';
+import PerAge from './PerAge';
+import { TimestampsContext } from '../context/TimestampsContext';
+
+const LAB = () => {
+  const { labTests, cases } = useContext(TimestampsContext);
+  const { data: labTestsTimestamp } = labTests;
+  const { data: casesTimestamp } = cases;
+  const dates = { 'lab-tests': labTestsTimestamp, cases: casesTimestamp };
+
+  return (
+    <Card summary="LAB" dates={dates} open={true}>
+      <Post postNumber={1}>
+        <PCR />
+        <HAT />
+        <ActiveCases />
+      </Post>
+    </Card>
+  );
+};
+
+const HOS = () => {
+  const { patients } = useContext(TimestampsContext);
+  const { data: patientsTimestamp } = patients;
+  const dates = { patients: patientsTimestamp };
+  return (
+    <Card summary="HOS" dates={dates} open={true}>
+      <Post postNumber={2}>
+        <Hospitalized />
+        <OnRespiratory />
+        <Care />
+        <Deceased />
+      </Post>
+    </Card>
+  );
+};
+
+const EPI = () => {
+  const { stats } = useContext(TimestampsContext);
+  const { data: patientsTimestamp } = stats;
+  const dates = { stats: patientsTimestamp };
+  return (
+    <Card summary="EPI" dates={dates} open={true}>
+      <Post postNumber={3}>
+        <PCR />
+        <HAT />
+        <ActiveCases />
+        <Vaccination />
+        <ComfirmedToDate />
+        <PerAge />
+        <Hospitalized />
+        <OnRespiratory />
+        <Care />
+        <Deceased />
+      </Post>
+    </Card>
+  );
+};
+
 function Posts() {
   return (
     <section>
+      <LAB />
       <br />
-      <Card summary="LAB" open={true}>
-        <Post postNumber={1}>
-          <PCR />
-          <HAT />
-          <ActiveCases />
-        </Post>
-      </Card>
+      <HOS />
       <br />
-      <Card summary="HOS">
-        <Post postNumber={2}>
-          <Hospitalized />
-          <OnRespiratory />
-          <Care />
-          <Deceased />
-        </Post>
-      </Card>
-      <br />
-      <Card summary="EPI">
-        <Post postNumber={3}>
-          <PCR />
-          <HAT />
-          <ActiveCases />
-          <Hospitalized />
-          <OnRespiratory />
-          <Care />
-          <Deceased />
-        </Post>
-      </Card>
+      <EPI />
       <br />
     </section>
   );
