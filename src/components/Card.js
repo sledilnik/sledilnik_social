5d19bbf5ff3e623dc88ca702969b5549dad5e79d
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-12 15:09:38 +0100

    feat(Card)_ +timestamps

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { formatRelative } from 'date-fns';
+import { sl } from 'date-fns/locale';
 
-function Card({ summary, children, open = false }) {
+function Card({ summary, dates = {}, children, open = false }) {
+  const datesOutput = Object.entries(dates).map(([path, ts], index) => (
+    <span key={ts + '-' + index}>
+      {path}:{' '}
+      {formatRelative(new Date(ts * 1000), new Date(), {
+        locale: sl,
+      })}{' '}
+    </span>
+  ));
+
   return (
     <details open={open}>
       <summary>
         <h2>{summary}</h2>
+        {datesOutput}
       </summary>
       {children}
     </details>
