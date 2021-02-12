e3ca12ca00e0865a29bf00624e9281ed52c04ae1
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-12 15:07:28 +0100

    fix(urlDict): one more day for patients endpoint

--- src/dicts/urlDict.js
+++ src/dicts/urlDict.js
@@ -13,18 +13,18 @@ export const API_URL = {
 };
 
 export const API_PARAMS = {
-  PATIENTS: { from: getISODateFrom(-2) },
+  PATIENTS: { from: getISODateFrom(-3) },
   STATS: { from: getISODateFrom(-4) },
 };
 export const TIMESTAMP_URL = {
-  STATS: `${BASE_TIMESTAMP_URL}/stats.csv.timestamp`,
-  PATIENTS: `${BASE_TIMESTAMP_URL}/patients.csv.timestamp`,
-  MUN: `${BASE_TIMESTAMP_URL}/municipality-active.csv.timestamp`,
   LAB_TESTS: `${BASE_TIMESTAMP_URL}/lab-tests.csv.timestamp`,
   CASES: `${BASE_TIMESTAMP_URL}/cases.csv.timestamp`,
-  HOSPITALS: `${BASE_TIMESTAMP_URL}/hospitals.csv.timestamp`,
-  ICU: `${BASE_TIMESTAMP_URL}/icu.csv.timestamp`,
-  VACCINATION: `${BASE_TIMESTAMP_URL}/vaccination.csv.timestamp`,
+  PATIENTS: `${BASE_TIMESTAMP_URL}/patients.csv.timestamp`,
+  STATS: `${BASE_TIMESTAMP_URL}/stats.csv.timestamp`,
+  // MUN: `${BASE_TIMESTAMP_URL}/municipality-active.csv.timestamp`,
+  // HOSPITALS: `${BASE_TIMESTAMP_URL}/hospitals.csv.timestamp`,
+  // ICU: `${BASE_TIMESTAMP_URL}/icu.csv.timestamp`,
+  // VACCINATION: `${BASE_TIMESTAMP_URL}/vaccination.csv.timestamp`,
 };
 
 export default {
