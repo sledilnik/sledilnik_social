a819bdd022ba5274efbab11f8971f778cd6f1b0e
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-12 15:13:18 +0100

    feat(components): +PerAge

---
+++ src/components/PerAge.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import PresentData from './PresentData';
+import { stats } from './dataDict';
+import getTranslatedData from '../utils/getTranslatedData';
+import { DataContext } from '../context/DataContext';
+
+// path patients
+const dataDict = stats.statePerAgeToDate;
+
+function PerAge({ data, ...props }) {
+  const translatedData = getTranslatedData(dataDict, data);
+
+  return <PresentData data={translatedData} props={props} />;
+}
+
+function withPerAgeHOC(Component) {
+  return ({ ...props }) => {
+    const { stats: hook } = useContext(DataContext);
+
+    if (hook.isLoading) {
+      return 'Loading....';
+    }
+
+    if (hook.data === null) {
+      return 'Null';
+    }
+
+    const sortedData = [...hook.data].sort(
+      (a, b) => b.dayFromStart - a.dayFromStart
+    );
+
+    const newProps = { ...props, data: sortedData };
+
+    return <Component {...newProps} />;
+  };
+}
+export default withPerAgeHOC(PerAge);
