746efc54c42d246d6f1af499c983d6d505425bdb
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-15 16:47:46 +0100

    feat(Card): +Logo

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,25 @@ import React from 'react';
 import { formatRelative } from 'date-fns';
 import { sl } from 'date-fns/locale';
 
+import sledilnikLogo from '../assets/sledilnik-logo.svg';
+
+const Logo = ({
+  className,
+  src = sledilnikLogo,
+  alt = 'logo',
+  width = '96',
+  height = '96',
+}) => (
+  <img
+    src={src}
+    alt={alt}
+    className={className}
+    width={width}
+    height={height}
+    style={{ display: 'block', margin: '0 auto' }}
+  />
+);
+
 function Card({ summary, dates = {}, children, open = false }) {
   const datesOutput = Object.entries(dates).map(([path, ts], index) => (
     <span key={ts + '-' + index}>
@@ -19,6 +38,7 @@ function Card({ summary, dates = {}, children, open = false }) {
         {datesOutput}
       </summary>
       {children}
+      <Logo />
     </details>
   );
 }
