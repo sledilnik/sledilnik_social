e44545dc2b946a2810b98f105c8a16bb617edc53
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-15 10:00:26 +0100

    refactor(DTD): separate only text and dataKeys

--- src/dicts/DataTranslateDict.js
+++ src/dicts/DataTranslateDict.js
@@ -1,109 +1,82 @@
 export const FBSummaryDict = {
   testsToday: [
-    {
-      prefix: 'PCR: ',
-      dataKeys: ['subValues', 'positive'],
-      suffix: ', ',
-      formatType: 'sign',
-    },
-    { prefix: 'testiranih: ', dataKeys: ['value'], suffix: ', ' },
-    {
-      prefix: 'delež pozitivnih: ',
-      dataKeys: ['subValues', 'percent'],
-      suffix: '.',
-      formatType: 'percent',
-      divide: 100,
-    },
+    { prefix: 'PCR: ' },
+    { dataKeys: ['subValues', 'positive'], formatType: 'sign' },
+    { prefix: ', testiranih: ' },
+    { dataKeys: ['value'] },
+    { prefix: ', delež pozitivnih: ' },
+    { dataKeys: ['subValues', 'percent'], formatType: 'percent', divide: 100 },
+    { prefix: '.' },
   ],
   testsTodayHAT: [
-    {
-      prefix: 'HAT: ',
-      dataKeys: ['subValues', 'positive'],
-      suffix: ', ',
-      formatType: 'sign',
-    },
-    {
-      prefix: 'testiranih: ',
-      dataKeys: ['value'],
-      suffix: ', ',
-    },
-    {
-      prefix: 'delež pozitivnih: ',
-      dataKeys: ['subValues', 'percent'],
-      suffix: '.',
-      formatType: 'percent',
-      divide: 100,
-    },
+    { prefix: 'HAT: ' },
+    { dataKeys: ['subValues', 'positive'], formatType: 'sign' },
+    { prefix: ', testiranih: ' },
+    { dataKeys: ['value'] },
+    { prefix: ', delež pozitivnih: ' },
+    { dataKeys: ['subValues', 'percent'], formatType: 'percent', divide: 100 },
+    { prefix: '.' },
   ],
   casesActive: [
+    { prefix: 'Aktivni primeri: ' },
+    { dataKeys: ['value'] },
     {
-      prefix: 'Aktivni primeri: ',
-      dataKeys: ['value'],
-      suffix: ' ',
-    },
-    {
-      prefix: '(',
+      prefix: ' (',
       dataKeys: ['subValues', 'in'],
       suffix: ', ',
       formatType: 'sign',
     },
     {
-      prefix: '',
       dataKeys: ['subValues', 'out'],
-      suffix: ').',
+      suffix: ')',
       formatType: 'sign',
       negative: true,
     },
+    { prefix: '.' },
   ],
   casesToDateSummary: [
-    {
-      prefix: 'Skupaj: ',
-      dataKeys: ['value'],
-      suffix: ' potrjenih primerov.',
-    },
+    { prefix: 'Skupaj: ' },
+    { dataKeys: ['value'] },
+    { prefix: ' potrjenih primerov.' },
   ],
   vaccinationSummary: [
-    { prefix: 'Cepljenih oseb: 💉', dataKeys: ['value'], suffix: ', ' },
-    { prefix: '💉💉', dataKeys: ['subValues', 'in'], suffix: '.' },
+    { prefix: 'Cepljenih oseb: 💉' },
+    { dataKeys: ['value'] },
+    { prefix: ', 💉💉', dataKeys: ['subValues', 'in'], suffix: '.' },
   ],
 };
 
 export const FBPatientsDict = {
   hospitalized: [
+    { prefix: 'Hospitalizirani: ' },
+    { dataKeys: ['0', 'total', 'inHospital', 'today'] },
     {
-      prefix: 'Hospitalizirani: ',
-      dataKeys: ['0', 'total', 'inHospital', 'today'],
-      suffix: ' ',
-    },
-    {
-      prefix: '(',
+      prefix: ' (',
       dataKeys: ['0', 'total', 'inHospital', 'in'],
-      suffix: ', ',
       formatType: 'sign',
     },
+    { prefix: ', ' },
     {
       prefix: '',
       dataKeys: ['0', 'total', 'inHospital', 'out'],
-      suffix: '), ',
+      suffix: ')',
       formatType: 'sign',
       negative: true,
     },
+    { prefix: ', EIT: ' },
+    { dataKeys: ['0', 'total', 'icu', 'today'] },
     {
-      prefix: 'EIT: ',
-      dataKeys: ['0', 'total', 'icu', 'today'],
-      suffix: ' ',
-    },
-    {
-      prefix: '(',
+      prefix: ' (',
       dataKeys: ['total', 'icu', 'today'],
       calculate: { what: 'diff', indexArray: [0, 1] },
-      suffix: ').',
+      suffix: ')',
       formatType: 'sign',
     },
+    { prefix: '.' },
   ],
   onRespiratory: [
+    { prefix: 'Na respiratorju: ' },
     {
-      prefix: 'Na respiratorju: ',
       dataKeys: ['total'],
       calculate: {
         what: 'sum',
@@ -112,39 +85,35 @@ export const FBPatientsDict = {
           ['niv', 'today'],
         ],
       },
-      suffix: ', ',
     },
-    {
-      prefix: 'intubirani: ',
-      dataKeys: ['0', 'total', 'critical', 'today'],
-      suffix: ' ',
-    },
+    { prefix: ', intubirani: ' },
+    { dataKeys: ['0', 'total', 'critical', 'today'], suffix: ' ' },
+    { prefix: ' ' },
     {
       prefix: '(',
       dataKeys: ['total', 'critical', 'today'],
       calculate: { what: 'diff', indexArray: [0, 1] },
-      suffix: '), ',
+      suffix: ')',
       formatType: 'sign',
     },
+    { prefix: ', neinvazivno: ' },
     {
-      prefix: 'neinvazivno: ',
       dataKeys: ['0', 'total', 'niv', 'today'],
-      suffix: ' ',
     },
+    { prefix: ' ' },
     {
       prefix: '(',
       dataKeys: ['total', 'niv', 'today'],
       calculate: { what: 'diff', indexArray: [0, 1] },
-      suffix: ').',
+      suffix: ')',
       formatType: 'sign',
     },
+    { prefix: '.' },
   ],
   care: [
-    {
-      prefix: 'Negovalne bolnišnice: ',
-      dataKeys: ['0', 'total', 'care', 'today'],
-      suffix: ' ',
-    },
+    { prefix: 'Negovalne bolnišnice: ' },
+    { dataKeys: ['0', 'total', 'care', 'today'] },
+    { prefix: ' ' },
     {
       prefix: '( ',
       dataKeys: ['0', 'total', 'care', 'in'],
@@ -154,23 +123,18 @@ export const FBPatientsDict = {
     {
       prefix: '',
       dataKeys: ['0', 'total', 'care', 'out'],
-      suffix: ').',
+      suffix: ')',
       formatType: 'sign',
       negative: true,
     },
+    { prefix: '.' },
   ],
   deceased: [
-    {
-      prefix: 'Umrli: ',
-      dataKeys: ['0', 'total', 'deceased', 'today'],
-      suffix: ', ',
-      formatType: 'sign',
-    },
-    {
-      prefix: 'skupaj: ',
-      dataKeys: ['0', 'total', 'deceased', 'toDate'],
-      suffix: '.',
-    },
+    { prefix: 'Umrli: ' },
+    { dataKeys: ['0', 'total', 'deceased', 'today'], formatType: 'sign' },
+    { prefix: ', skupaj: ' },
+    { dataKeys: ['0', 'total', 'deceased', 'toDate'] },
+    { prefix: '.' },
   ],
   inHospitals: [{ prefix: 'Stanje po bolnišnicah:' }],
 };
@@ -178,13 +142,12 @@ export const FBPatientsDict = {
 const perAge = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((acc, item) => {
   acc = [
     ...acc,
-    {
-      dataKeys: ['0', 'statePerAgeToDate', item, 'ageFrom'],
-      suffix: item === 9 ? '' : '-',
-    },
+    { dataKeys: ['0', 'statePerAgeToDate', item, 'ageFrom'], style: null },
+    { prefix: item === 9 ? '' : '-' },
     item === 9
       ? { prefix: '+' }
-      : { dataKeys: ['0', 'statePerAgeToDate', item, 'ageTo'], suffix: ' ' },
+      : { dataKeys: ['0', 'statePerAgeToDate', item, 'ageTo'], style: null },
+    { prefix: ' ' },
     {
       prefix: '(',
       dataKeys: ['allToDate'],
@@ -195,18 +158,14 @@ const perAge = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((acc, item) => {
           ['2', 'statePerAgeToDate', item],
         ],
       },
-      suffix: item === 9 ? ').' : '), ',
+      suffix: ')',
     },
+    { prefix: item === 9 ? '.' : ', ' },
   ];
   return acc;
 }, []);
 export const FBStatsDict = {
-  statePerAgeToDate: [
-    {
-      prefix: 'Potrjeni primeri po starosti: ',
-    },
-    ...perAge,
-  ],
+  statePerAgeToDate: [{ prefix: 'Potrjeni primeri po starosti: ' }, ...perAge],
 };
 
 export const FBMunicipalitiesDict = {
