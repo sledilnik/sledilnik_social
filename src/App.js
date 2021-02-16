626691b805cd026e62f1ef13cf4531f1b2cc0849
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-16 16:39:19 +0100

    refactor(App): -SocialChanger
    SocialChanger is in Header

--- src/App.js
+++ src/App.js
@@ -1,49 +1,27 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Posts from './components/Posts';
 import { DataProvider } from './context/DataContext';
-import { SocialProvider, SocialContext } from './context/SocialContext';
+import { SocialProvider } from './context/SocialContext';
 import { TimestampsProvider } from './context/TimestampsContext';
 import Header from './components/Header';
 
 import './App.css';
 import Footer from './components/Footer';
 
-const SocialChanger = () => {
-  const context = useContext(SocialContext);
-
-  const socialHandler = () => {
-    const button = document.getElementById('social-changer');
-    if (context.social === 'FB') {
-      button.innerText = 'Prikaži FB ikone';
-      context.setSocial('TW');
-      return;
-    }
-    button.innerText = 'Prikaži TW ikone';
-    context.setSocial('FB');
-  };
-
-  return (
-    <button id="social-changer" onClick={socialHandler}>
-      Prikaži TW ikone
-    </button>
-  );
-};
-
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        <TimestampsProvider>
-          <SocialProvider>
-            <SocialChanger />
+      <SocialProvider>
+        <Header />
+        <main>
+          <TimestampsProvider>
             <DataProvider>
               <Posts />
             </DataProvider>
-          </SocialProvider>
-        </TimestampsProvider>
-      </main>
-      <Footer />
+          </TimestampsProvider>
+        </main>
+        <Footer />
+      </SocialProvider>
     </div>
   );
 }
