363ef099c3907b8e56acfed4f1ff4358acc6944f
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-16 13:39:10 +0100

    feat(compnonets)(shared): +Backdrop

---
+++ src/components/shared/Backdrop.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Backdrop.css';
+
+function Backdrop({ children, className }) {
+  return <div className={`Backdrop ${className}`}>{children}</div>;
+}
+
+export default Backdrop;
