ef3ea9f27b9daf9e0511fb43db3900b6a253edcf
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-17 18:13:06 +0100

    style(Footer): free icons credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,6 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="Footer">
-      Icon by <a href="https://freeicons.io/profile/714">Raj Dev</a> on{' '}
-      <a href="https://freeicons.io">freeicons.io</a>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -16,6 +14,10 @@ function Footer() {
       >
         <path d="M12 4.419c-2.826-5.695-11.999-4.064-11.999 3.27 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-7.327-9.17-8.972-12-3.27z" />
       </svg>
+      <div className="free-icons-credit">
+        Copy icon by <a href="https://freeicons.io/profile/714">Raj Dev</a> on{' '}
+        <a href="https://freeicons.io">freeicons.io</a>
+      </div>
     </footer>
   );
 }
