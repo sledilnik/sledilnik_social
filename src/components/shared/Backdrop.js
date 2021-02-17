77a3340ac1cc9fb1186733eb7b5e0677285fc196
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-17 10:51:31 +0100

    feat(Backdrop): default prop className value
    set to empty string

--- src/components/shared/Backdrop.js
+++ src/components/shared/Backdrop.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Backdrop.css';
 
-function Backdrop({ children, className }) {
+function Backdrop({ children, className = '' }) {
   return <div className={`Backdrop ${className}`}>{children}</div>;
 }
 
