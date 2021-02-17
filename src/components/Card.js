fe234f9c5c29236b0c08c61125c2be0459b68c68
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-17 10:39:02 +0100

    feat(Card): + open PopOut

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Card.css';
 
@@ -6,6 +6,7 @@ import { formatRelative } from 'date-fns';
 import { sl } from 'date-fns/locale';
 
 import sledilnikLogo from '../assets/sledilnik-logo.svg';
+import PopOut from './shared/PopOut';
 
 const Logo = ({
   src = sledilnikLogo,
@@ -23,26 +24,61 @@ const Logo = ({
 );
 
 function Card({ id, summary, dates = {}, children, open = false }) {
+  const [clipboard, setClipboard] = useState(null);
+  const [showPopOut, setShowPopOut] = useState(false);
+
+  useEffect(() => {
+    const close = e => {
+      if (e.keyCode === 27) {
+        setShowPopOut(false);
+      }
+    };
+    window.addEventListener('keydown', close);
+    return () => window.removeEventListener('keydown', close);
+  }, []);
+
   const sortedDates = [...Object.values(dates)].sort((a, b) => b - a);
 
   const noDates = Object.values(dates).every(item => item === null);
 
   const relativeDate = noDates ? (
-    <span style={{ opacity: 0, marginLeft: '24px' }}>loading...</span>
+    <span style={{ opacity: 0, marginLeft: '24px' }}>ni podatka</span>
   ) : (
     formatRelative(new Date(sortedDates[0] * 1000), new Date(), {
       locale: sl,
     })
   );
 
+  const copyHandler = event => {
+    const buttonId = event.target.id;
+    const postId = buttonId.replace('copy-card', 'post');
+    const article = document.getElementById(postId);
+    let text = article.innerText.replace(/(\r\n|\r|\n){2,}/g, '\n');
+    text = text.slice(-1) === '\n' ? text.slice(0, -1) : text;
+    setClipboard(text);
+    setShowPopOut(true);
+  };
+
+  const buttonId = 'copy-' + id;
+
   return (
     <details id={id} className="Card" open={open}>
       <summary>
-        <h2>{summary}</h2>
         <div>Osveženo: {relativeDate}</div>
+        <h2>{summary}</h2>
+        <button id={buttonId} onClick={copyHandler}>
+          V odložišče
+        </button>
       </summary>
       {children}
       <Logo />
+      <PopOut open={showPopOut} className={'popout-backdrop'}>
+        <textarea
+          rows="10"
+          defaultValue={clipboard}
+          style={{ width: ' 100%', resize: 'vertical' }}
+        />
+      </PopOut>
     </details>
   );
 }
