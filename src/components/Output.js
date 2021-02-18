3571afd1574115b84268a67176a6a9a99b8b5321
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-18 08:06:03 +0100

    feat(Output): change color if wrong date

--- src/components/Output.js
+++ src/components/Output.js
@@ -9,21 +9,23 @@ function Output({
   noArrow = false,
   ...props
 }) {
-  const { social, kindOfData } = props;
+  const { social, kindOfData, wrongDate } = props;
   const texts = {
     ...defaultTexts,
     ...TextsDict.FB[kindOfData],
     ...TextsDict[social][kindOfData],
   };
 
+  const color = wrongDate ? 'var(--red)' : 'initial';
+
   const output = Object.values(texts).map((item, index) => (
-    <span key={`${keyTitle}-${index}`}>
+    <span key={`${keyTitle}-${index}`} style={{ color }}>
       {item}
       <span style={{ fontWeight: 700 }}>{data[`value${index + 1}`]}</span>
     </span>
   ));
 
-  return <DataRow dataString={output} noArrow={noArrow} />;
+  return <DataRow dataString={output} noArrow={noArrow} markFail={wrongDate} />;
 }
 
 export default Output;
