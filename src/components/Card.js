fcafe741388ef17b2e29cdd11d7cb4b8a5bcdc0f
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-19 14:23:18 +0100

    refactor(Card): implement ToClipboard component

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ import { sl } from 'date-fns/locale';
 
 import sledilnikLogo from '../assets/sledilnik-logo.svg';
 import copyIcon from '../assets/svg/copy.svg';
-import PopOut from './shared/PopOut';
+import ToClipboard from './shared/ToClipboard';
 
 function Card({ id, summary, dates = {}, children, open = false }) {
   const detailsRef = useRef();
@@ -26,18 +26,7 @@ function Card({ id, summary, dates = {}, children, open = false }) {
     return () => window.removeEventListener('keydown', close);
   }, []);
 
-  const sortedDates = [...Object.values(dates)].sort((a, b) => b - a);
-
-  const noDates = Object.values(dates).every(item => item === null);
-  const relativeDate = noDates ? (
-    <span style={{ opacity: 0, marginLeft: '24px' }}>ni podatka</span>
-  ) : (
-    formatRelative(new Date(sortedDates[0] * 1000), new Date(), {
-      locale: sl,
-    })
-  );
-
-  const copyHandler = event => {
+  const openPopOutHandler = event => {
     const buttonId = event.target.id;
     const postId = buttonId.replace('copy-card', 'post');
     const article = document.getElementById(postId);
@@ -50,29 +39,6 @@ function Card({ id, summary, dates = {}, children, open = false }) {
     text = text.slice(-1) === '\n' ? text.slice(0, -1) : text;
     setClipboard(text);
     setShowPopOut(true);
-  };
-
-  const selectAndCopy = textarea => {
-    textarea.select();
-    textarea.setSelectionRange(0, textarea.value.length);
-    document.execCommand('copy');
-  };
-
-  const closeHandler = async () => {
-    const textarea = document.getElementById('textarea-copy');
-    textarea.value = '';
-    navigator.clipboard && (await navigator.clipboard.writeText(''));
-    !navigator.clipboard && selectAndCopy(textarea);
-    setClipboard('');
-    setShowPopOut(false);
-  };
-
-  const toClipboardHandler = async () => {
-    const textarea = document.getElementById('textarea-copy');
-    navigator.clipboard &&
-      (await navigator.clipboard.writeText(textarea.value));
-    !navigator.clipboard && selectAndCopy(textarea);
-    setShowPopOut(false);
   };
 
   const onDetailsClick = event => {
@@ -137,7 +103,7 @@ function Card({ id, summary, dates = {}, children, open = false }) {
             src={copyIcon}
             width={16}
             height={16}
-            onClick={copyHandler}
+            onClick={openPopOutHandler}
             alt="copy icon"
           />
         </div>
@@ -150,26 +116,12 @@ function Card({ id, summary, dates = {}, children, open = false }) {
         height="48"
         style={{ display: 'block', margin: '0 auto' }}
       />
-      <PopOut open={showPopOut}>
-        <div className="popout-container">
-          <div className="textarea-container">
-            <textarea
-              id="textarea-copy"
-              readOnly={true}
-              rows="10"
-              defaultValue={clipboard}
-              style={{ width: ' 100%', resize: 'none' }}
-            />
-          </div>
-          <div className="button-container">
-            <button onClick={toClipboardHandler}>V odložišče</button>
-          </div>
-        </div>
-        <div className="close" onClick={closeHandler}>
-          <div className="line line-1"></div>
-          <div className="line line-2"></div>
-        </div>
-      </PopOut>
+      <ToClipboard
+        open={showPopOut}
+        defaultValue={clipboard}
+        setClipboard={setClipboard}
+        setShowPopOut={setShowPopOut}
+      />
     </details>
   );
 }
