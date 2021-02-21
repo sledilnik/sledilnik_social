230b33f23f6eb7d670f3f315bd33ff861f67c73d
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-21 08:08:28 +0100

    refacto(CancelButton): show name in devTools

--- src/components/shared/CancelButton.js
+++ src/components/shared/CancelButton.js
@@ -3,24 +3,30 @@ import React, { useCallback, useEffect } from 'react';
 import './CancelButton.css';
 
 function CancelButton({ handleClick }) {
-  const handleClickMemo = useCallback(() => handleClick(), [handleClick]);
-
-  useEffect(() => {
-    const close = e => {
-      if (e.keyCode === 27) {
-        handleClickMemo();
-      }
-    };
-    window.addEventListener('keydown', close);
-    return () => window.removeEventListener('keydown', close);
-  }, [handleClickMemo]);
-
   return (
-    <div className="CancelButton top-right" onClick={handleClickMemo}>
+    <div className="CancelButton top-right" onClick={handleClick}>
       <div className="line line-1"></div>
       <div className="line line-2"></div>
     </div>
   );
 }
 
-export default CancelButton;
+function withCancelButtonHOC(Component) {
+  const WithCancelButton = ({ handleClick, ...props }) => {
+    const handleClickMemo = useCallback(() => handleClick(), [handleClick]);
+    useEffect(() => {
+      const close = e => {
+        if (e.keyCode === 27) {
+          handleClickMemo();
+        }
+      };
+      window.addEventListener('keydown', close);
+      return () => window.removeEventListener('keydown', close);
+    }, [handleClickMemo]);
+
+    return <Component handleClick={handleClick} {...props} />;
+  };
+  return WithCancelButton;
+}
+
+export default withCancelButtonHOC(CancelButton);
