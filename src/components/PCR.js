997be6ef2de35bdbf49fde08b0ef05642a9036de
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-23 11:21:10 +0100

    feat(PCR): +FetchBoundary

--- src/components/PCR.js
+++ src/components/PCR.js
@@ -12,6 +12,7 @@ import {
 } from '../utils/formatNumber';
 
 import Output from './Output';
+import FetchBoundary from './FetchBoundary';
 
 const TextsDict = {
   FB: {
@@ -35,53 +36,54 @@ const TextsDict = {
 
 const defaultTexts = TextsDict.FB.default;
 
+const getPCRData = data => {
+  const {
+    value,
+    subValues: { positive, percent },
+  } = data.testsToday;
+
+  const kindOfData = value && !!positive && !!percent ? 'default' : 'onlyValue';
+
+  const newData =
+    kindOfData === 'default'
+      ? {
+          value1: formatNumberWithSign(positive),
+          value2: formatNumber(value),
+          value3: formatPercentage(percent / 100),
+        }
+      : { value1: formatNumber(value) };
+
+  const wrongDate = differenceInDays(new Date(), getDate(data.testsToday)) > 1;
+
+  return { data: newData, kindOfData, wrongDate };
+};
+
+function PCR({ hook, outputProps, ...props }) {
+  return (
+    <FetchBoundary hook={hook}>
+      <Output {...outputProps} />
+    </FetchBoundary>
+  );
+}
+
 function withPCR_HOC(Component) {
-  const PCR = ({ ...props }) => {
+  const WithPCR = ({ ...props }) => {
     const { summary: hook } = useContext(DataContext);
     const { social } = useContext(SocialContext);
+    const data = hook.data && getPCRData(hook.data);
 
-    if (hook.isLoading) {
-      return 'Loading....';
-    }
-
-    if (hook.data === null) {
-      return 'Null';
-    }
-
-    const {
-      value,
-      subValues: { positive, percent },
-    } = hook.data.testsToday;
-
-    const kindOfData =
-      value && !!positive && !!percent ? 'default' : 'onlyValue';
-
-    const newData =
-      kindOfData === 'default'
-        ? {
-            value1: formatNumberWithSign(positive),
-            value2: formatNumber(value),
-            value3: formatPercentage(percent / 100),
-          }
-        : { value1: formatNumber(value) };
-
-    const wrongDate =
-      differenceInDays(new Date(), getDate(hook.data.testsToday)) > 1;
-
-    const newProps = {
-      ...props,
-      data: newData,
+    const outputProps = {
       social,
-      kindOfData,
+      ...data,
       defaultTexts,
       TextsDict,
       keyTitle: 'PCR',
-      wrongDate,
     };
-    return <Component {...newProps} />;
+
+    return <Component hook={hook} outputProps={outputProps} {...props} />;
   };
 
-  return PCR;
+  return WithPCR;
 }
 
-export default withPCR_HOC(Output);
+export default withPCR_HOC(PCR);
