4117503ac0544184eb67f6871ecaef52752e8ab2
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-23 11:20:19 +0100

    feat(ActiveCases): +FetchBoundary

--- src/components/ActiveCases.js
+++ src/components/ActiveCases.js
@@ -8,6 +8,7 @@ import { formatNumberWithSign } from './../utils/formatNumber';
 import Output from './Output';
 import { differenceInDays } from 'date-fns';
 import { getDate } from '../utils/dates';
+import FetchBoundary from './FetchBoundary';
 
 // path summary
 
@@ -28,51 +29,50 @@ const defaultTexts = TextsDict.FB.default;
 
 const Brackets = ({ children }) => <>({children})</>;
 
+const getActiveCasesData = data => {
+  const {
+    value,
+    subValues: { in: casesIn, out },
+  } = data.casesActive;
+  const kindOfData = 'default';
+  const newData = {
+    value1: formatNumber(value),
+    value2: (
+      <Brackets>
+        {formatNumberWithSign(casesIn)}, {formatNumberWithSign(-out)}
+      </Brackets>
+    ),
+  };
+  const wrongDate = differenceInDays(new Date(), getDate(data.casesActive)) > 1;
+
+  return { data: newData, kindOfData, wrongDate };
+};
+
+function ActiveCases({ hook, outputProps, ...props }) {
+  return (
+    <FetchBoundary hook={hook}>
+      <Output {...outputProps} />
+    </FetchBoundary>
+  );
+}
+
 function withActiveCases_HOC(Component) {
-  const ActiveCases = ({ ...props }) => {
+  const WithActiveCases = ({ ...props }) => {
     const { summary: hook } = useContext(DataContext);
     const { social } = useContext(SocialContext);
+    const data = hook.data && getActiveCasesData(hook.data);
 
-    if (hook.isLoading) {
-      return 'Loading....';
-    }
-
-    if (hook.data === null) {
-      return 'Null';
-    }
-
-    const {
-      value,
-      subValues: { in: casesIn, out },
-    } = hook.data.casesActive;
-
-    const kindOfData = 'default';
-
-    const newData = {
-      value1: formatNumber(value),
-      value2: (
-        <Brackets>
-          {formatNumberWithSign(casesIn)}, {formatNumberWithSign(-out)}
-        </Brackets>
-      ),
-    };
-
-    const wrongDate =
-      differenceInDays(new Date(), getDate(hook.data.casesActive)) > 1;
-
-    const newProps = {
-      ...props,
-      data: newData,
+    const outputProps = {
       social,
-      kindOfData,
+      ...data,
       defaultTexts,
       TextsDict,
       keyTitle: 'ActiveCases',
-      wrongDate,
     };
 
-    return <Component {...newProps} />;
+    return <Component hook={hook} outputProps={outputProps} {...props} />;
   };
-  return ActiveCases;
+
+  return WithActiveCases;
 }
-export default withActiveCases_HOC(Output);
+export default withActiveCases_HOC(ActiveCases);
