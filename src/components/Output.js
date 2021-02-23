02c18358bf16b29bd96a94ef074ccca3023638d2
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-23 16:01:16 +0100

    feat(Output): +withOutputHOC

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import DataRow from './DataRow';
 
-function Output({
-  data,
-  defaultTexts,
-  TextsDict,
-  keyTitle,
-  noArrow = false,
-  ...props
-}) {
-  const { social, kindOfData, isWrongDate } = props;
-  const texts = {
-    ...defaultTexts,
-    ...TextsDict.FB[kindOfData],
-    ...TextsDict[social][kindOfData],
-  };
-
-  const color = isWrongDate ? 'var(--red)' : 'initial';
-
-  const output = Object.values(texts).map((item, index) => (
-    <span key={`${keyTitle}-${index}`} style={{ color }}>
-      {item}
-      <span style={{ fontWeight: 700 }}>{data[`value${index + 1}`]}</span>
-    </span>
-  ));
-
+function Output({ output, noArrow, markFail, ...props }) {
   return (
-    <DataRow dataString={output} noArrow={noArrow} markFail={isWrongDate} />
+    <DataRow
+      dataString={output}
+      noArrow={noArrow}
+      markFail={markFail}
+      {...props}
+    />
   );
 }
 
-export default Output;
+function withOutputHOC(Component) {
+  const WithOutput = props => {
+    const { kindOfData, TextsDict, social, defaultTexts, ...rest1 } = props;
+
+    const DefaultDict = kindOfData ? TextsDict.FB[kindOfData] : {};
+    const SocialDict =
+      social || kindOfData ? TextsDict[social][kindOfData] : {};
+
+    const texts = {
+      ...defaultTexts,
+      ...DefaultDict,
+      ...SocialDict,
+    };
+
+    const { isWrongDate, keyTitle, data, ...rest2 } = rest1;
+
+    const color = isWrongDate ? 'var(--red)' : 'initial';
+
+    const output = Object.values(texts).map((item, index) => (
+      <span key={`${keyTitle}-${index}`} style={{ color }}>
+        {item}
+        <span style={{ fontWeight: 700 }}>{data[`value${index + 1}`]}</span>
+      </span>
+    ));
+
+    return <Component output={output} {...rest2} />;
+  };
+  return WithOutput;
+}
+export default withOutputHOC(Output);
