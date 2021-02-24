3c2053930e312fc214deb1fbbb8504c58799d5da
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-24 18:37:14 +0100

    feat(Deceased): +FetchBoundary

--- src/components/Deceased.js
+++ src/components/Deceased.js
@@ -3,71 +3,48 @@ import { differenceInDays } from 'date-fns';
 import { getDate } from '../utils/dates';
 
 import { DataContext } from '../context/DataContext';
-import { SocialContext } from '../context/SocialContext';
 
 import { formatNumberWithSign, formatNumber } from '../utils/formatNumber';
 
 import Output from './Output';
+import FetchBoundary from './FetchBoundary';
 
 // path patients
 
-const TextsDict = {
-  FB: {
-    default: {
-      text1: 'Umrli: ',
-      text2: ', skupaj: ',
-      text3: '.',
-    },
-    onlyValue: {},
-  },
-  TW: {
-    default: {},
-    onValue: {},
-  },
+function Deceased({ hook, outputProps, ...props }) {
+  return (
+    <FetchBoundary hook={hook}>
+      <Output {...outputProps} />
+    </FetchBoundary>
+  );
+}
+
+const getDeceasedData = data => {
+  const sortedData = [...data].sort((a, b) => b.dayFromStart - a.dayFromStart);
+
+  const kindOfData = 'default';
+
+  const newData = {
+    value1: formatNumberWithSign(sortedData[0].total.deceased.today),
+    value2: formatNumber(sortedData[0].total.deceased.toDate),
+  };
+
+  const isWrongDate = differenceInDays(new Date(), getDate(sortedData[0])) > 0;
+  return { data: newData, kindOfData, isWrongDate };
 };
 
-const defaultTexts = TextsDict.FB.default;
-
 function withDeceasedHOC(Component) {
-  const Deceased = ({ ...props }) => {
+  const WithDeceased = ({ ...props }) => {
     const { patients: hook } = useContext(DataContext);
-    const { social } = useContext(SocialContext);
+    const data = hook.data && getDeceasedData(hook.data);
 
-    if (hook.isLoading) {
-      return 'Loading....';
-    }
-
-    if (hook.data === null) {
-      return 'Null';
-    }
-
-    const sortedData = [...hook.data].sort(
-      (a, b) => b.dayFromStart - a.dayFromStart
-    );
-
-    const kindOfData = 'default';
-
-    const newData = {
-      value1: formatNumberWithSign(sortedData[0].total.deceased.today),
-      value2: formatNumber(sortedData[0].total.deceased.toDate),
-    };
-
-    const isWrongDate =
-      differenceInDays(new Date(), getDate(sortedData[0])) > 0;
-
-    const newProps = {
-      ...props,
-      data: newData,
-      social,
-      kindOfData,
-      defaultTexts,
-      TextsDict,
+    const outputProps = {
+      ...data,
       keyTitle: 'Deceased',
-      isWrongDate,
     };
 
-    return <Component {...newProps} />;
+    return <Component hook={hook} outputProps={outputProps} {...props} />;
   };
-  return Deceased;
+  return WithDeceased;
 }
-export default withDeceasedHOC(Output);
+export default withDeceasedHOC(Deceased);
