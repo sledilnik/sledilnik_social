e6e0d3a458c8afefcaa5aa95d4227134708f6c8e
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-24 18:37:48 +0100

    feat(ConfirmedToDate): +FetchBoundary

--- src/components/ConfirmedToDate.js
+++ src/components/ConfirmedToDate.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { differenceInDays } from 'date-fns';
 
 import { DataContext } from '../context/DataContext';
-import { SocialContext } from '../context/SocialContext';
 
 import { formatNumber } from '../utils/formatNumber';
 import { getDate } from '../utils/dates';
@@ -11,23 +10,6 @@ import Output from './Output';
 import FetchBoundary from './FetchBoundary';
 
 // path summary
-
-const TextsDict = {
-  FB: {
-    default: {
-      text1: 'Skupaj: ',
-      text2: '',
-      text3: ' potrjenih primerov.',
-    },
-    onlyValue: {},
-  },
-  TW: {
-    default: {},
-    onValue: {},
-  },
-};
-
-const defaultTexts = TextsDict.FB.default;
 
 const getConfirmedToDateData = data => {
   const { value } = data.casesToDateSummary;
@@ -52,15 +34,11 @@ function ConfirmedToData({ hook, outputProps, ...props }) {
 function withConfirmedToDate_HOC(Component) {
   const WithConfirmedToDate = ({ ...props }) => {
     const { summary: hook } = useContext(DataContext);
-    const { social } = useContext(SocialContext);
     const data = hook.data && getConfirmedToDateData(hook.data);
 
     const outputProps = {
-      social,
       ...data,
-      defaultTexts,
-      TextsDict,
-      keyTitle: 'Vaccination',
+      keyTitle: 'ConfirmedToDate',
     };
 
     return <Component hook={hook} outputProps={outputProps} {...props} />;
