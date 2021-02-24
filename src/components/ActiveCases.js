abec5364f3894187ee81af6298bced12a3913074
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-02-24 18:36:24 +0100

    feat(ActiveCases): +FetchBoundary

--- src/components/ActiveCases.js
+++ src/components/ActiveCases.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { DataContext } from '../context/DataContext';
-import { SocialContext } from '../context/SocialContext';
 
 import { formatNumber } from '../utils/formatNumber';
 import { formatNumberWithSign } from './../utils/formatNumber';
@@ -11,22 +10,6 @@ import { getDate } from '../utils/dates';
 import FetchBoundary from './FetchBoundary';
 
 // path summary
-
-const TextsDict = {
-  FB: {
-    default: {
-      text1: 'Aktivni primeri: ',
-      text2: ' ',
-      text3: '.',
-    },
-  },
-  TW: {
-    default: {},
-  },
-};
-
-const defaultTexts = TextsDict.FB.default;
-
 const Brackets = ({ children }) => <>({children})</>;
 
 const getActiveCasesData = data => {
@@ -60,14 +43,10 @@ function ActiveCases({ hook, outputProps, ...props }) {
 function withActiveCases_HOC(Component) {
   const WithActiveCases = ({ ...props }) => {
     const { summary: hook } = useContext(DataContext);
-    const { social } = useContext(SocialContext);
     const data = hook.data && getActiveCasesData(hook.data);
 
     const outputProps = {
-      social,
       ...data,
-      defaultTexts,
-      TextsDict,
       keyTitle: 'ActiveCases',
     };
 
