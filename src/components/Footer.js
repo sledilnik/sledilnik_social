8ec0868b43e5a80d165e75ce5c10b483668c7e93
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-05 16:49:52 +0100

    refactor(Footer): delete icons credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,10 +13,6 @@ function Footer() {
       >
         <path d="M12 4.419c-2.826-5.695-11.999-4.064-11.999 3.27 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-7.327-9.17-8.972-12-3.27z" />
       </svg>
-      <div className="free-icons-credit">
-        Copy icon by <a href="https://freeicons.io/profile/714">Raj Dev</a> on{' '}
-        <a href="https://freeicons.io">freeicons.io</a>
-      </div>
     </footer>
   );
 }
