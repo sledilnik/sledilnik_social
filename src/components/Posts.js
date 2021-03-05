788ab6e687681173efe65a25105fc0c66d02572c
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-05 16:48:39 +0100

    feat(Posts): +refreshHandler

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,15 +2,16 @@ import React, { useContext, useRef } from 'react';
 
 import './Posts.css';
 
+import { TimestampsContext } from '../context/TimestampsContext';
+import { DataContext } from '../context/DataContext';
+
 import Card from './Card';
 import Post from './Post';
-
-import PerAge from './PerAge';
-import { TimestampsContext } from '../context/TimestampsContext';
-import InHospitals from './InHospitals';
-import Municipalities from './Municipalities';
 import Summary from './Summary';
 import Patients from './Patients';
+import PerAge from './PerAge';
+import InHospitals from './InHospitals';
+import Municipalities from './Municipalities';
 
 const LAB = () => {
   const ref = useRef();
@@ -19,8 +20,23 @@ const LAB = () => {
   const { data: casesTimestamp } = cases;
   const dates = { 'lab-tests': labTestsTimestamp, cases: casesTimestamp };
 
+  const { summary } = useContext(DataContext);
+
+  const refreshHandler = () => {
+    labTests.refetch();
+    cases.refetch();
+    summary.refetch();
+  };
+
   return (
-    <Card postRef={ref} title="LAB" dates={dates} open noCount={false}>
+    <Card
+      postRef={ref}
+      title="LAB"
+      dates={dates}
+      open
+      noCount={false}
+      refreshHandler={refreshHandler}
+    >
       <Post forwardedRef={ref} id="post-lab" postNumber={1}>
         <Summary title="PCR" />
         <Summary title="HAT" />
@@ -35,8 +51,21 @@ const HOS = () => {
   const { patients } = useContext(TimestampsContext);
   const { data: patientsTimestamp } = patients;
   const dates = { patients: patientsTimestamp };
+
+  const { patients: patientsDataHook } = useContext(DataContext);
+
+  const refreshHandler = () => {
+    patients.refetch();
+    patientsDataHook.refetch();
+  };
   return (
-    <Card postRef={ref} title="HOS" dates={dates} noCount={false}>
+    <Card
+      postRef={ref}
+      title="HOS"
+      dates={dates}
+      noCount={false}
+      refreshHandler={refreshHandler}
+    >
       <Post forwardedRef={ref} id="post-hos" postNumber={2}>
         <Patients title="Hospitalized" />
         <Patients title="OnRespiratory" />
@@ -64,8 +93,29 @@ const EPI = () => {
     patients: patientsTimestamp,
     'municipalities-active': munActiveTimestamp,
   };
+
+  const dataHooks = useContext(DataContext);
+
+  const refreshHandler = () => {
+    stats.refetch();
+    labTests.refetch();
+    cases.refetch();
+    patients.refetch();
+    munActive.refetch();
+    dataHooks.stats.refetch();
+    dataHooks.summary.refetch();
+    dataHooks.patients.refetch();
+    dataHooks.hospitalsList.refetch();
+    dataHooks.municipalities.refetch();
+  };
   return (
-    <Card postRef={ref} title="EPI" dates={dates} open={false}>
+    <Card
+      postRef={ref}
+      title="EPI"
+      dates={dates}
+      open={false}
+      refreshHandler={refreshHandler}
+    >
       <Post forwardedRef={ref} id="post-epi" postNumber={3}>
         <Summary title="PCR" />
         <Summary title="HAT" />
