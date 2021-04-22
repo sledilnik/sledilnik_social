d5b9515ccf03f38ca813f2fa0528f3026ed33916
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-22 10:36:23 +0200

    feat(components): +Screenshots

---
+++ src/components/Screenshots.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Screenshot from './Screenshot';
+import './Screenshots.css';
+
+function Screenshots() {
+  return (
+    <div className="Screenshots">
+      <div className="cards">
+        <Screenshot params={{ type: 'card', screen: 'testsToday' }} />
+        <Screenshot params={{ type: 'card', screen: 'testsTodayHAT' }} />
+        <Screenshot params={{ type: 'card', screen: 'casesActive' }} />
+        <Screenshot params={{ type: 'card', screen: 'casesAvg7Days' }} />
+        <Screenshot params={{ type: 'card', screen: 'hospitalizedCurrent' }} />
+        <Screenshot params={{ type: 'card', screen: 'icuCurrent' }} />
+        <Screenshot params={{ type: 'card', screen: 'deceasedToDate' }} />
+        <Screenshot params={{ type: 'card', screen: 'vaccinationSummary' }} />
+      </div>
+      <div className="charts">
+        <Screenshot params={{ type: 'chart', screen: 'Patients' }} />
+        <Screenshot params={{ type: 'chart', screen: 'IcuPatients' }} />
+      </div>
+    </div>
+  );
+}
+
+export default Screenshots;
