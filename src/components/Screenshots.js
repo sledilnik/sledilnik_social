97e9ce2a7ed654b75e317564d585380ce92e521c
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-22 10:47:22 +0200

    feat(Screenshots): separate cards by post

--- src/components/Screenshots.js
+++ src/components/Screenshots.js
@@ -9,10 +9,14 @@ function Screenshots() {
         <Screenshot params={{ type: 'card', screen: 'testsToday' }} />
         <Screenshot params={{ type: 'card', screen: 'testsTodayHAT' }} />
         <Screenshot params={{ type: 'card', screen: 'casesActive' }} />
-        <Screenshot params={{ type: 'card', screen: 'casesAvg7Days' }} />
+      </div>
+      <div className="cards">
         <Screenshot params={{ type: 'card', screen: 'hospitalizedCurrent' }} />
         <Screenshot params={{ type: 'card', screen: 'icuCurrent' }} />
         <Screenshot params={{ type: 'card', screen: 'deceasedToDate' }} />
+      </div>
+      <div className="cards">
+        <Screenshot params={{ type: 'card', screen: 'casesAvg7Days' }} />
         <Screenshot params={{ type: 'card', screen: 'vaccinationSummary' }} />
       </div>
       <div className="charts">
