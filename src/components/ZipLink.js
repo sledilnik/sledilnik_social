67f649b3faf830999d56dd416b6a6526485e99b2
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-22 15:36:19 +0200

    feat(components): +ZipLink

---
+++ src/components/ZipLink.js
@@ -0,0 +1,46 @@
+import JSZip from 'jszip';
+import FileSaver from 'file-saver';
+import React from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+function ZipLink() {
+  const testsToday = useLocalStorage(null, 'testsToday');
+  const testsTodayHAT = useLocalStorage(null, 'testsTodayHAT');
+  const casesActive = useLocalStorage(null, 'casesActive');
+  const hospitalizedCurrent = useLocalStorage(null, 'hospitalizedCurrent');
+  const icuCurrent = useLocalStorage(null, 'icuCurrent');
+  const deceasedToDate = useLocalStorage(null, 'deceasedToDate');
+  const casesAvg7Days = useLocalStorage(null, 'casesAvg7Days');
+  const vaccinationSummary = useLocalStorage(null, 'vaccinationSummary');
+  const Patients = useLocalStorage(null, 'Patients');
+  const IcuPatients = useLocalStorage(null, 'IcuPatients');
+
+  const screenshotsStateHooks = {
+    testsToday,
+    testsTodayHAT,
+    casesActive,
+    hospitalizedCurrent,
+    icuCurrent,
+    deceasedToDate,
+    casesAvg7Days,
+    vaccinationSummary,
+    Patients,
+    IcuPatients,
+  };
+
+  const onClickHandler = async () => {
+    const zip = new JSZip();
+
+    for (let [key, item] of Object.entries(screenshotsStateHooks)) {
+      zip.file(key + '.png', item[0], { base64: true });
+    }
+
+    const content = await zip.generateAsync({ type: 'blob' });
+
+    FileSaver.saveAs(content, 'screenshots.zip');
+  };
+
+  return <button onClick={onClickHandler}>Download ZIP</button>;
+}
+
+export default ZipLink;
