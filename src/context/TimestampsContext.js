9997b98d252112f6bbf4cbc6028ada04572843ec
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-22 15:04:16 +0200

    feat(TimestampsContext): stats ts to localStoraga
    if new ts set all img data in local storage to null

--- src/context/TimestampsContext.js
+++ src/context/TimestampsContext.js
@@ -1,6 +1,7 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect } from 'react';
 import { TIMESTAMP_URL } from '../dicts/URL_Dict';
 import useFetch from '../hooks/useFetch';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 export const TimestampsContext = createContext();
 
@@ -12,6 +13,49 @@ export const TimestampsProvider = ({ children }) => {
   const labTests = useFetch(TIMESTAMP_URL.LAB_TESTS, params);
   const cases = useFetch(TIMESTAMP_URL.CASES, params);
   const munActive = useFetch(TIMESTAMP_URL.MUN, params);
+
+  const [value, setValue] = useLocalStorage(stats.data, 'statsTimestamp');
+  const testsToday = useLocalStorage(null, 'testsToday');
+  const testsTodayHAT = useLocalStorage(null, 'testsTodayHAT');
+  const casesActive = useLocalStorage(null, 'casesActive');
+  const hospitalizedCurrent = useLocalStorage(null, 'hospitalizedCurrent');
+  const icuCurrent = useLocalStorage(null, 'icuCurrent');
+  const deceasedToDate = useLocalStorage(null, 'deceasedToDate');
+  const casesAvg7Days = useLocalStorage(null, 'casesAvg7Days');
+  const vaccinationSummary = useLocalStorage(null, 'vaccinationSummary');
+  const Patients = useLocalStorage(null, 'Patients');
+  const IcuPatients = useLocalStorage(null, 'IcuPatients');
+
+  const screenshotsStateHooks = [
+    testsToday,
+    testsTodayHAT,
+    casesActive,
+    hospitalizedCurrent,
+    icuCurrent,
+    deceasedToDate,
+    casesAvg7Days,
+    vaccinationSummary,
+    Patients,
+    IcuPatients,
+  ];
+
+  const ts = stats.data;
+
+  useEffect(() => {
+    if (value < ts) {
+      setValue(ts);
+    }
+
+    if (value === null || value < ts) {
+      const screenshotsSetState = screenshotsStateHooks.map(item => {
+        return item[1];
+      });
+
+      for (let setState of screenshotsSetState) {
+        setState(null);
+      }
+    }
+  }, [value, ts, setValue, screenshotsStateHooks]);
 
   return (
     <TimestampsContext.Provider
