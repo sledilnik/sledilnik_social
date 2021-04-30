b43ca247e9cff4b85329763a6cf89db9855a645a
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-30 16:10:05 +0200

    feat(Screenshots): + 3 zip buttons
    all zip files has images with translated filenames

--- src/components/Screenshots.js
+++ src/components/Screenshots.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ScreenshotGetUseLocalStorage, {
-  ScreenshotNames,
-} from '../dicts/ScreenshotGetUseLocalStorage';
+import { ScreenshotNames } from '../dicts/ScreenshotGetUseLocalStorage';
 import Screenshot from './Screenshot';
 import './Screenshots.css';
 import ZipLink from './ZipLink';
@@ -15,6 +13,27 @@ const typesDict = {
   CARDS,
   MULTICARD,
 };
+
+const translatedNames = Object.entries(ScreenshotNames).reduce(
+  (acc, [type, names]) => {
+    const translated = names.reduce((nestedAcc, name) => {
+      const indexOf = name.indexOf('_');
+
+      const types = typesDict[type.toUpperCase()];
+      const [_name, _custom] = name.split('_');
+
+      let text =
+        indexOf < 0
+          ? types[name].text
+          : types[_name].customCharts[_custom].text;
+      nestedAcc[name] = text;
+      return nestedAcc;
+    }, {});
+    acc = { ...acc, ...translated };
+    return acc;
+  },
+  {}
+);
 
 function ScreenshotsByType({ title, type, ...props }) {
   const names = ScreenshotNames[type.toUpperCase()];
@@ -48,11 +67,33 @@ function ScreenshotsByType({ title, type, ...props }) {
 }
 
 function Screenshots() {
-  const filenames = Object.keys(ScreenshotGetUseLocalStorage);
   return (
     <div className="Screenshots">
       <div className="buttons-container">
-        <ZipLink filenames={filenames} />
+        <ZipLink
+          text="Vse"
+          filenames={ScreenshotNames}
+          translated={translatedNames}
+          zipName={'All_screeenshots'}
+        />
+        <ZipLink
+          text="Multi kartice"
+          filenames={ScreenshotNames.MULTICARD}
+          translated={translatedNames}
+          zipName={'multicards_screeenshots'}
+        />
+        <ZipLink
+          text="Grafi"
+          filenames={ScreenshotNames.CHARTS}
+          translated={translatedNames}
+          zipName={'charts_screeenshots'}
+        />
+        <ZipLink
+          text="Kartice"
+          filenames={ScreenshotNames.CARDS}
+          translated={translatedNames}
+          zipName={'cards_screeenshots'}
+        />
       </div>
       <ScreenshotsByType title="Multi" type="multicard" className="multicard" />
       <ScreenshotsByType
