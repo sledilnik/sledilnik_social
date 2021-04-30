ac6115d77505480fec32d48d7abb4ef4d6dc7f7e
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-30 14:04:33 +0200

    feat(dicts): +MulticardDict

---
+++ src/dicts/MulticardDict.js
@@ -0,0 +1,14 @@
+export default {
+  LAB: {
+    name: 'LAB',
+    text: 'LAB',
+  },
+  HOS: {
+    name: 'HOS',
+    text: 'HOS',
+  },
+  ALL: {
+    name: 'ALL',
+    text: 'Vse kartice',
+  },
+};
