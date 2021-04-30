e3aff30ed23b3ecf466ad8e52418eaa861d24e90
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-30 14:09:56 +0200

    feat(Screenshots): +ScrenshotByType

--- src/components/Screenshots.js
+++ src/components/Screenshots.js
@@ -1,8 +1,51 @@
 import React from 'react';
-import ScreenshotGetUseLocalStorage from '../dicts/ScreenshotGetUseLocalStorage';
+import ScreenshotGetUseLocalStorage, {
+  ScreenshotNames,
+} from '../dicts/ScreenshotGetUseLocalStorage';
 import Screenshot from './Screenshot';
 import './Screenshots.css';
 import ZipLink from './ZipLink';
+
+import CHARTS from '../dicts/ChartsDict';
+import CARDS from '../dicts/CardsDict';
+import MULTICARD from '../dicts/MulticardDict';
+
+const typesDict = {
+  CHARTS,
+  CARDS,
+  MULTICARD,
+};
+
+function ScreenshotsByType({ title, type, ...props }) {
+  const names = ScreenshotNames[type.toUpperCase()];
+
+  const screenshots = names.map(name => {
+    const indexOf = name.indexOf('_');
+
+    const types = typesDict[type.toUpperCase()];
+    const [_name, _custom] = name.split('_');
+
+    let text =
+      indexOf < 0 ? types[name].text : types[_name].customCharts[_custom].text;
+
+    return (
+      <div key={`${type}-${name}`} className="screenshot-container">
+        <Screenshot
+          params={{ type: type, screen: name }}
+          captionTop
+          captionText={text}
+        />
+      </div>
+    );
+  });
+
+  return (
+    <div className={props.className}>
+      <h2>{title || type}</h2>
+      <div className="screenshots-container">{screenshots}</div>
+    </div>
+  );
+}
 
 function Screenshots() {
   const filenames = Object.keys(ScreenshotGetUseLocalStorage);
@@ -11,110 +54,13 @@ function Screenshots() {
       <div className="buttons-container">
         <ZipLink filenames={filenames} />
       </div>
-      <h2>MULTI</h2>
-      <div className="cards">
-        <Screenshot
-          params={{ type: 'multicard', screen: 'LAB' }}
-          captionBottom
-        />
-        <Screenshot
-          params={{ type: 'multicard', screen: 'HOS' }}
-          captionBottom
-        />
-        <Screenshot
-          params={{ type: 'multicard', screen: 'ALL' }}
-          captionBottom
-        />
-      </div>
-      <h2>LAB</h2>
-      <div className="cards">
-        <Screenshot
-          params={{ type: 'card', screen: 'testsToday' }}
-          captionBottom
-        />
-        <Screenshot
-          params={{ type: 'card', screen: 'testsTodayHAT' }}
-          captionBottom
-        />
-        <Screenshot
-          params={{ type: 'card', screen: 'casesActive' }}
-          captionBottom
-        />
-      </div>
-      <h2>HOS</h2>
-      <div className="cards">
-        <Screenshot
-          params={{ type: 'card', screen: 'hospitalizedCurrent' }}
-          captionBottom
-        />
-        <Screenshot
-          params={{ type: 'card', screen: 'icuCurrent' }}
-          captionBottom
-        />
-        <Screenshot
-          params={{ type: 'card', screen: 'deceasedToDate' }}
-          captionBottom
-        />
-      </div>
-      <h2>Ostalo</h2>
-      <div className="cards">
-        <Screenshot
-          params={{ type: 'card', screen: 'casesAvg7Days' }}
-          captionBottom
-        />
-        <Screenshot
-          params={{ type: 'card', screen: 'vaccinationSummary' }}
-          captionBottom
-        />
-      </div>
-      <div className="charts">
-        <div className="chart">
-          <h2>Hospitalizirani</h2>
-          <Screenshot
-            params={{ type: 'chart', screen: 'Patients' }}
-            captionBottom
-          />
-        </div>
-        <div className="chart">
-          <h2>Intezivna terapija</h2>
-          <Screenshot
-            params={{ type: 'chart', screen: 'IcuPatients' }}
-            captionBottom
-          />
-        </div>
-        <div className="chart">
-          <h2>Primeri po občinah</h2>
-          <Screenshot
-            params={{ type: 'chart', screen: 'Municipalities' }}
-            captionBottom
-          />
-        </div>
-        <div className="chart">
-          <h2>Zemljevid po občinah - tedenski prirast</h2>
-          <Screenshot
-            params={{ type: 'chart', screen: 'Map', custom: 'weeklyGrowth' }}
-            captionBottom
-          />
-        </div>
-        <div className="chart">
-          <h2>Zemljevid po občinah - absolutno 1 dan</h2>
-          <Screenshot
-            params={{ type: 'chart', screen: 'Map', custom: 'absolute1Day' }}
-            captionBottom
-          />
-        </div>
-        <div className="chart">
-          <h2>Zemljevid po občinah - porazdelitev 1 dan</h2>
-          <Screenshot
-            params={{
-              type: 'chart',
-              screen: 'Map',
-              custom: 'distribution1Day',
-            }}
-            captionBottom
-          />
-        </div>
-      </div>
+      <ScreenshotsByType title="Multi" type="multicard" className="multicard" />
+      <ScreenshotsByType
+        title="Posamezne kartice"
+        type="cards"
+        className="cards"
+      />
+      <ScreenshotsByType title="Grafi" type="charts" className="charts" />
     </div>
   );
 }
