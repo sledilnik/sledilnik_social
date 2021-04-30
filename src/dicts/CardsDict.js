848238ae87b4dff5ed2336fcc8b74ee53a5e6434
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-30 14:03:57 +0200

    feat(dicts): +CardsDict

---
+++ src/dicts/CardsDict.js
@@ -0,0 +1,34 @@
+export default {
+  testsToday: {
+    name: 'testsToday',
+    text: 'Testi na dan: PCR',
+  },
+  testsTodayHAT: {
+    name: 'testsToday',
+    text: 'Testi na dan: HAT',
+  },
+  casesActive: {
+    name: 'casesActive',
+    text: 'Aktivni primeri',
+  },
+  hospitalizedCurrent: {
+    name: 'hospitalizedCurrent',
+    text: 'Hospitalizirani',
+  },
+  icuCurrent: {
+    name: 'icuCurrent',
+    text: 'V intenzivni terapiji',
+  },
+  deceasedToDate: {
+    name: 'deceasedToDate',
+    text: 'Umrli',
+  },
+  casesAvg7Days: {
+    name: 'casesAvg7Days',
+    text: 'Novi primeri',
+  },
+  vaccinationSummary: {
+    name: 'vaccinationSummary',
+    text: 'Cepljenih oseb',
+  },
+};
