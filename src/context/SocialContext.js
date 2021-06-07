0a3e4c1562762b491a95f1170009dd3aabbfd48b
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-06-07 09:58:17 +0200

    feat(SocialContext): default social TW

--- src/context/SocialContext.js
+++ src/context/SocialContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const SocialContext = createContext();
 
 export const SocialProvider = ({ children }) => {
-  const [social, setSocial] = useState('FB');
+  const [social, setSocial] = useState('TW');
 
   return (
     <SocialContext.Provider value={{ social, setSocial }}>
